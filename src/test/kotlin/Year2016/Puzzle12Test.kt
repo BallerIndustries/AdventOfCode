c7331f7265b8847097991817bbea0fd2a6e281f1
Angus Cheng <anguscheng5@gmail.com>
2018-10-19 01:02:22 +0800

    all commands are handled, now for the big bang

--- src/test/kotlin/Year2016/Puzzle12Test.kt
+++ src/test/kotlin/Year2016/Puzzle12Test.kt
@@ -1,5 +1,6 @@
 package Year2016
 
+import Year2016.Puzzle12.State
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -36,22 +37,51 @@ class Puzzle12Test {
     @Test
     fun `can parse jump command`() {
         val commands = puzzle.parseCommandText("jnz a 2")
-        assertEquals(commands, listOf(Puzzle12.Jump("a", "2")))
+        assertEquals(commands, listOf(Puzzle12.JumpIfNotZero("a", "2")))
     }
 
     @Test
     fun `state after a copy value command`() {
-        val initialState = Puzzle12.State()
+        val initialState = State()
         val currentState = Puzzle12.Copy("12", "a").execute(initialState)
-        assertEquals(currentState, Puzzle12.State(1, mapOf("a" to 12)))
+        assertEquals(currentState, State(1, mapOf("a" to 12, "b" to 0, "c" to 0, "d" to 0)))
     }
 
     @Test
     fun `state after a copy register command`() {
-        val initialState = Puzzle12.State()
+        val initialState = State()
         val state1 = Puzzle12.Copy("12", "a").execute(initialState)
         val state2 = Puzzle12.Copy("a", "b").execute(state1)
-        assertEquals(state2, Puzzle12.State(2, mapOf("a" to 12, "b" to 12)))
+        assertEquals(state2, State(2, mapOf("a" to 12, "b" to 12, "c" to 0, "d" to 0)))
+    }
+
+    @Test
+    fun `state after an increment register command`() {
+        val initialState = State()
+        val state1 = Puzzle12.IncrementRegister("a").execute(initialState)
+        assertEquals(state1, State(1, mapOf("a" to 1, "b" to 0, "c" to 0, "d" to 0)))
+    }
+
+    @Test
+    fun `state after an decrement register command`() {
+        val initialState = State()
+        val state1 = Puzzle12.DecrementRegister("a").execute(initialState)
+        assertEquals(state1, State(1, mapOf("a" to -1, "b" to 0, "c" to 0, "d" to 0)))
+    }
+
+    @Test
+    fun `state after jump command, when register is zero`() {
+        val initialState = State()
+        val state1 = Puzzle12.JumpIfNotZero("a", "7").execute(initialState)
+        assertEquals(state1, State(1, mapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)))
+    }
+
+    @Test
+    fun `state after jump command, when register is not zero`() {
+        val initialState = State()
+        val stateOne = Puzzle12.IncrementRegister("a").execute(initialState)
+        val stateTwo = Puzzle12.JumpIfNotZero("a", "7").execute(stateOne)
+        assertEquals(stateTwo, State(8, mapOf("a" to 1, "b" to 0, "c" to 0, "d" to 0)))
     }
 }
 
@@ -64,7 +94,7 @@ class Puzzle12 {
                 "cpy" -> Copy(tmp[1], tmp[2])
                 "inc" -> IncrementRegister(tmp[1])
                 "dec" -> DecrementRegister(tmp[1])
-                "jnz" -> Jump(tmp[1], tmp[2])
+                "jnz" -> JumpIfNotZero(tmp[1], tmp[2])
                 else -> throw RuntimeException("Sorry don't support that command. command = ${tmp[0]}")
             }
         }
@@ -84,23 +114,37 @@ class Puzzle12 {
 
     data class IncrementRegister(val sourceRegister: String) : Command {
         override fun execute(state: State): State {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val newProgramCounter = state.programCounter + 1
+            val newRegisters = state.registers + mapOf(sourceRegister to state.getValueOrValueFromRegister(sourceRegister) + 1)
+            return State(newProgramCounter, newRegisters)
         }
     }
 
     data class DecrementRegister(val sourceRegister: String) : Command {
         override fun execute(state: State): State {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val newProgramCounter = state.programCounter + 1
+            val newRegisters = state.registers + mapOf(sourceRegister to state.getValueOrValueFromRegister(sourceRegister) - 1)
+            return State(newProgramCounter, newRegisters)
         }
     }
 
-    data class Jump(val conditionalValueOrRegister: String, val deltaValueOrRegister: String) : Command {
+    data class JumpIfNotZero(val conditionalValueOrRegister: String, val deltaValueOrRegister: String) : Command {
         override fun execute(state: State): State {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            if (state.getValueOrValueFromRegister(conditionalValueOrRegister) == 0) {
+                return State(state.programCounter + 1, state.registers)
+            }
+            else {
+                val deltaValue = state.getValueOrValueFromRegister(deltaValueOrRegister)
+                return State(state.programCounter + deltaValue, state.registers)
+            }
         }
     }
 
-    data class State(val programCounter: Int = 0, val registers: Map<String, Int> = mapOf()) {
+    data class State(val programCounter: Int = 0, val registers: Map<String, Int> = initialState) {
+        companion object {
+            val initialState = mapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
+        }
+
         fun getValueOrValueFromRegister(valueOrRegister: String): Int {
             return if (valueOrRegister.toIntOrNull() != null) valueOrRegister.toInt() else registers[valueOrRegister]!!
         }
