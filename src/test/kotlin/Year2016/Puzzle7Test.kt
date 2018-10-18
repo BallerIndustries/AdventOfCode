fd47fcba93731b02afb5bead25ffd751279c926a
Angus Cheng <anguscheng5@gmail.com>
2018-10-18 23:10:09 +0800

    Bam! Puzzle 7 done, champagne all around!

--- src/test/kotlin/Year2016/Puzzle7Test.kt
+++ src/test/kotlin/Year2016/Puzzle7Test.kt
@@ -10,7 +10,7 @@ class Puzzle7Test {
     val puzzle = Puzzle7()
 
     @Test
-    fun `abba outside brackets, supports TLS` () {
+    fun `abba outside brackets, supports TLS`() {
         val result = puzzle.supportsTLS("abba[mnop]qrst")
         assertTrue(result)
     }
@@ -34,6 +34,18 @@ class Puzzle7Test {
     }
 
     @Test
+    fun `ioxxoj should contain an abba pattern`() {
+        val result = puzzle.textHasAbbaPattern("ioxxoj")
+        assertTrue(result)
+    }
+
+    @Test
+    fun `abba should contain an abba pattern`() {
+        val result = puzzle.textHasAbbaPattern("abba")
+        assertTrue(result)
+    }
+
+    @Test
     fun `can pull out bracketed text into an array of strings`() {
         val input = "ipwwgkvjagdlqkoxlat[widqrotdnywnnbdn]rtviotwkbdqpggscdt[jzbcukafvquuxiu]ctmziuyofwucvdvjom"
         val result = puzzle.getBracketedText(input)
@@ -51,7 +63,39 @@ class Puzzle7Test {
     fun `puzzle part a`() {
         val input = Puzzle1Test::class.java.getResource("/2016/puzzle7.txt").readText()
         val count = puzzle.countTLSIpAddresses(input)
-        Assert.assertEquals(count, 0)
+        Assert.assertEquals(count, 110)
+    }
+
+    @Test
+    fun `aba{bab}xyz supports SSL`() {
+        assertTrue(puzzle.supportsSSL("aba[bab]xyz"))
+    }
+
+    @Test
+    fun `xyx{xyx}xyx does not support SSL`() {
+        assertFalse(puzzle.supportsSSL("xyx[xyx]xyx"))
+    }
+
+    @Test
+    fun `aaa{kek}eke supports SSL`() {
+        assertTrue(puzzle.supportsSSL("aaa[kek]eke"))
+    }
+
+    @Test
+    fun `zazbz{bzb}cdb supports SSL`() {
+        assertTrue(puzzle.supportsSSL("zazbz[bzb]cdb"))
+    }
+
+    @Test
+    fun `dsfoisjdfi j`() {
+        puzzle.supportsSSL("txxplravpgztjqcw[txgmmtlhmqpmmwp]bmhfgpmafxqwtrpr[inntmjmgqothdzfqgxq]cvtwvembpvdmcvk")
+    }
+
+    @Test
+    fun `puzzle part b`() {
+        val input = Puzzle1Test::class.java.getResource("/2016/puzzle7.txt").readText()
+        val count = puzzle.countSSLIpAddresses(input)
+        Assert.assertEquals(count, 242)
     }
 }
 
@@ -66,12 +110,36 @@ class Puzzle7 {
         return false
     }
 
-    private fun textHasAbbaPattern(text: String): Boolean {
-        text.forEachIndexed { index, _ ->
-            if (index + 4 < text.length - 1 && hasAbbaPattern(text.substring(index, index + 4))) return true
-        }
+    fun supportsSSL(ipAddress: String): Boolean {
+        val bracketedTextList = getBracketedText(ipAddress)
+        val nonBracketedTextList = getUnbracketedText(ipAddress)
 
-        return false
+        val bracketedAbaPatterns = bracketedTextList
+                .flatMap { getSubstrings(it, 3) }
+                .filter(this::hasAbaPattern)
+                .toSet()
+
+        val nonBracketedAbaPatterns = nonBracketedTextList
+                .flatMap { getSubstrings(it, 3) }
+                .filter(this::hasAbaPattern)
+                .map { "${it[1]}${it[0]}${it[1]}" }
+                .toSet()
+
+        return bracketedAbaPatterns.any { nonBracketedAbaPatterns.contains(it) }
+    }
+
+    fun textHasAbbaPattern(text: String): Boolean {
+        return getSubstrings(text, 4).any { hasAbbaPattern(it) }
+    }
+
+    private fun getSubstrings(text: String, size: Int): List<String> {
+        return text.mapIndexed { index, _ -> text.substring(index, Math.min(index + size, text.length)) }
+                .filter { it.length == size }
+    }
+
+    private fun hasAbaPattern(threeCharacterString: String): Boolean {
+        if (threeCharacterString.length != 3) throw RuntimeException("Angry, was expecting a three character string. threeCharacterString.length = ${threeCharacterString.length}")
+        return threeCharacterString[0] == threeCharacterString[2] && !threeCharacterString.all { it == threeCharacterString[0] }
     }
 
     private fun hasAbbaPattern(fourCharacterString: String): Boolean {
@@ -81,6 +149,10 @@ class Puzzle7 {
 
     fun countTLSIpAddresses(input: String): Int {
         return input.split('\n').filter(this::supportsTLS).count()
+    }
+
+    fun countSSLIpAddresses(input: String): Int {
+        return input.split("\r\n").filter(this::supportsSSL).count()
     }
 
     fun getUnbracketedText(input: String): List<String> {
