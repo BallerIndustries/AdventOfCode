6130eb3ddca5f51177079fe0a70ff28cfe339f39
anguruso <baller.industries@hotmail.com>
2018-12-05 19:31:09 +0800

    Day 5

--- src/test/kotlin/Year2018/Puzzle5Test.kt
+++ src/test/kotlin/Year2018/Puzzle5Test.kt
@@ -11,22 +11,65 @@ class Puzzle5Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(0, result)
+        assertEquals(11108, result.length)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(0, result)
+        assertEquals(5094, result)
+    }
+
+    @Test
+    fun `collapse test`() {
+        val collapsed = puzzle.collapse("aA")
+        assertEquals(collapsed, "")
+    }
+
+    @Test
+    fun example() {
+        val collapsed = puzzle.solveOne("dabAcCaCBAcCcaDA")
+        assertEquals("dabCBAcaDA", collapsed)
     }
 }
 
 class Puzzle5 {
-    fun solveOne(puzzleText: String): Int {
-        return 1
+    fun solveOne(puzzleText: String): String {
+        var previous = puzzleText
+        var current = collapse(puzzleText)
+
+        while (previous.length != current.length) {
+            previous = current
+            current = collapse(current)
+        }
+
+        return current
     }
 
     fun solveTwo(puzzleText: String): Int {
-        return 2
+        val dog = ('a'..'z').map { char ->
+
+            val newText = puzzleText
+                    .replace(char.toString(), "")
+                    .replace(char.toUpperCase().toString(), "")
+
+            char to solveOne(newText).length
+        }
+
+        return dog.minBy { it.second }!!.second
     }
+
+    fun collapse(jur: String): String {
+        var buffer = jur
+
+        ('a'..'z').forEach {
+            val pattern = "$it${it.toUpperCase()}"
+            val reversedPattern = pattern.reversed()
+            buffer = buffer.replace(Regex(pattern), "")
+            buffer = buffer.replace(Regex(reversedPattern), "")
+        }
+
+        return buffer
+    }
+
 }
