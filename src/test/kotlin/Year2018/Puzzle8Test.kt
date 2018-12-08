777395e6c9561cb92abf7c97ae482938b214f953
anguruso <baller.industries@hotmail.com>
2018-12-08 17:58:53 +0800

    not easy

--- src/test/kotlin/Year2018/Puzzle8Test.kt
+++ src/test/kotlin/Year2018/Puzzle8Test.kt
@@ -1,38 +1,75 @@
 package Year2018
 
 import junit.framework.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
-import java.util.*
 
 class Puzzle8Test {
     val puzzleText = this::class.java.getResource(
             "/2018/puzzle8.txt").readText()
     val puzzle = Puzzle8()
 
-
-        @Test
+    @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals("a", result)
+        assertEquals(46829, puzzle.sumMetaData(result))
     }
+
+    @Test
+    fun `puzzle part b`() {
+        val result = puzzle.solveOne(puzzleText)
+//        puzzle.pruneKids(result)
+
+        // Find all nodes where the number of children does not match teh child count
+//        val childCountMismatch = result.filter { it.childCount != it.children.count() }
+//        val childListLarger = result.filter { it.children.count() > it.childCount }
+//        val metaDataCountMismatch = result.filter { it.metaDataCount != it.metaData.count() }
 //
-//    @Test
-//    fun `puzzle part b`() {
-//        val result = puzzle.solveTwo(puzzleText)
-//        assertEquals("b", result)
-//    }
+//        assertEquals(0, metaDataCountMismatch.count())
+//        assertEquals(0, childCountMismatch.count())
+//        assertEquals(0, childListLarger.count())
+//
+
+        val sum = puzzle.sumNodesPartTwo(result[0])
+        assertEquals(37450, sum)
+
+
+        //println("Fucking cunttsss!!!")
+
+
+    }
+
+    @Test
+    fun `there should be no childCount mismatches`() {
+        val oneChild = "1 1"
+        val result = puzzle.solveOne("$oneChild $oneChild 0 1 100 200 300")
+        val childCountMismatch = result.filter { it.childCount != it.children.size }
+
+        assertEquals(0, childCountMismatch.count())
+    }
 
     @Test
     fun `example`() {
-        val result = puzzle.solveOne("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+        val nodes = puzzle.solveOne("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+        val assertableNodes = nodes.map { it.toAssertable() }
+
         val expected = listOf(
-                Node("A", 2, 3),
-                Node("B", 0, 3),
-                Node("C", 1, 1),
-                Node("D", 0, 1)
+            AssertableNode("A", 2, 3, mutableListOf(1, 1, 2), mutableListOf("B", "C", "D")),
+            AssertableNode("B", 0, 3, mutableListOf(10, 11, 12), mutableListOf()),
+            AssertableNode("C", 1, 1, mutableListOf(2), mutableListOf("D")),
+            AssertableNode("D", 0, 1, mutableListOf(99), mutableListOf())
         )
 
-        assertEquals(expected, result)
+        assertEquals(expected, assertableNodes)
+        assertEquals(138, puzzle.sumMetaData(nodes))
+    }
+
+    @Test
+    fun `example part two`() {
+        val nodes = puzzle.solveOne("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+        val sum = puzzle.sumNodesPartTwo(nodes[0])
+        assertEquals(66, sum)
+
     }
 
     @Test
@@ -139,7 +176,22 @@ class Puzzle8Test {
         assertEquals(11, actual)
     }
 
-    data class Node(val name: String, val childCount: Int, val metaDataCount: Int)
+    data class AssertableNode(
+        val name: String, val childCount: Int, val metaDataCount: Int,
+        val metaData: MutableList<Int> = mutableListOf(),
+        val childNames: MutableList<String> = mutableListOf()
+    )
+
+    data class Node(val name: String, val childCount: Int, val metaDataCount: Int,
+        val metaData: MutableList<Int> = mutableListOf(),
+        var children: MutableList<Node> = mutableListOf()
+    ) {
+
+        fun toAssertable(): AssertableNode {
+            return AssertableNode(name, childCount, metaDataCount, metaData, children.map { it.name }.toMutableList())
+        }
+    }
+
 
     class Puzzle8 {
         fun solveOne(puzzleText: String): List<Node> {
@@ -161,14 +213,16 @@ class Puzzle8Test {
                 // Will have to come back to this jerk to sort out its meta data entries.
                 if (node.childCount > 0) {
                     val (children, indexAfterChildren) = createChildren(nodeName, node.childCount, numbers.subList(index, numbers.lastIndex))
+                    node.children.addAll(children)
                     nodes.addAll(children)
                     index += indexAfterChildren
                 }
 
-                index += metaDataCount
+                // Add in the meta data
+                (index until index + metaDataCount).forEach { node.metaData.add(numbers[it]) }
 
+                index += metaDataCount
                 nodeName = nodes.maxBy { it.name }!!.name[0] + 1
-//                println(nodeName)
             }
 
             return nodes
@@ -179,38 +233,72 @@ class Puzzle8Test {
             var nodeName = parentName + 1
             val nodes = mutableListOf<Node>()
             var processedAtThisLevel = 0
+            val nodesAtThisLevel = mutableListOf<Node>()
 
             while (processedAtThisLevel < parentsChildCount) {
                 val childCount = numbers[index]
                 val metaDataCount = numbers[index + 1]
 
-//                println("name = $nodeName childCount = $childCount metaDataCount = $metaDataCount")
-                nodes.add(Node(nodeName.toString(), childCount, metaDataCount))
+                val node = Node(nodeName.toString(), childCount, metaDataCount)
+                nodes.add(node)
+
+                nodesAtThisLevel.add(node)
+
                 processedAtThisLevel++
                 index += 2
 
-//                if (nodes.count() > 1) println(nodes.count() )
-
                 if (childCount == 0) {
+                    (index until index + metaDataCount).forEach { node.metaData.add(numbers[it]) }
                     index += metaDataCount
                 }
                 else {
                     // Need to move the index forward after processing the children
                     val (children, indexDelta) = createChildren(nodeName, childCount, numbers.subList(index, numbers.lastIndex))
                     nodes.addAll(children)
-                    index += indexDelta + metaDataCount
+                    node.children.addAll(children)
+
+                    index += indexDelta
+
+                    (index until index + metaDataCount).forEach { node.metaData.add(numbers[it]) }
+
+                    index += metaDataCount
                 }
 
                 nodeName++
             }
 
-            nodes.sortBy { it.name }
-            return nodes to index
+            if (nodesAtThisLevel.size != parentsChildCount) throw RuntimeException("FUCKK!!!")
+
+            nodesAtThisLevel.sortBy { it.name }
+            return nodesAtThisLevel to index
+        }
+
+        fun pruneKids(nodes: List<Node>) {
+            nodes.forEach { node -> node.children = node.children.subList(0, node.childCount) }
+        }
+
+        fun sumMetaData(nodes: List<Node>): Int {
+            return nodes.sumBy { node -> node.metaData.sum() }
         }
 
 
         fun solveTwo(puzzleText: String): String {
             return ""
+        }
+
+        fun sumNodesPartTwo(node: Node): Int {
+            if (node.children.isEmpty()) {
+                return node.metaData.sum()
+            }
+
+            val normalisedIndexes = node.metaData.map { it - 1 }
+                .filter { it < node.childCount }
+
+            return normalisedIndexes.sumBy { indexToChild ->
+
+                val child = node.children[indexToChild]
+                sumNodesPartTwo(child)
+            }
         }
     }
 }
