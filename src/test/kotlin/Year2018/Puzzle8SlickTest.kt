a1e6b31315d183878d8359d4ccb011dfe9b23447
anguruso <baller.industries@hotmail.com>
2018-12-08 23:31:44 +0800

    super slick puzzle8

---
+++ src/test/kotlin/Year2018/Puzzle8SlickTest.kt
@@ -0,0 +1,77 @@
+package Year2018
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class Puzzle8SlickTest {
+    val puzzleText = this::class.java.getResource("/2018/puzzle8.txt").readText()
+    val puzzle = Puzzle8()
+
+    @Test
+    fun `puzzle part a`() {
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(46829, result)
+    }
+
+    @Test
+    fun `puzzle part b`() {
+        val result = puzzle.solveTwo(puzzleText)
+        assertEquals(37450, result)
+    }
+
+    class Puzzle8 {
+        private var numbers: List<Int> = listOf()
+        private var index = 0
+
+        private fun nextInt(): Int {
+            index++
+            return numbers[index - 1]
+        }
+
+        fun solveOne(puzzleText: String): Int {
+            numbers = puzzleText.split(" ").map { it.toInt() }
+            val root = readTree()
+            return sumMetaData(root)
+        }
+
+        fun solveTwo(puzzleText: String): Int {
+            numbers = puzzleText.split(" ").map { it.toInt() }
+            val root = readTree()
+
+            return sumPartTwo(root)
+        }
+
+        private fun sumPartTwo(node: Node): Int {
+            if (node.children.isEmpty()) return node.metadata.sum()
+
+            return node.metadata.map { it - 1 }
+                .filter { it < node.children.size }
+                .map { indexToChild -> node.children[indexToChild] }
+                .sumBy { child -> sumPartTwo(child) }
+        }
+
+        data class Node(val children: List<Node>, val metadata: List<Int>)
+
+        private fun readTree(): Node {
+            val numChildren = nextInt()
+            val numMetadata = nextInt()
+            val children = (0 until numChildren).map { readTree() }
+            val metadata = (0 until numMetadata).map { nextInt() }
+
+            return Node(children, metadata)
+        }
+
+        private fun sumMetaData(node: Node): Int {
+            var ans = 0
+            ans += node.metadata.sum()
+            ans += node.children.sumBy { sumMetaData(it) }
+
+            return ans
+        }
+    }
+}
+
+
+
+
+
