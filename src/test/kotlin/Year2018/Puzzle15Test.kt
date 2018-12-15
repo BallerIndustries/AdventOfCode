adb59e4fb16e7c12f2d8773b43cc1e19937e2beb
Angus Cheng <anguscheng5@gmail.com>
2018-12-15 15:27:01 +0800

    15 WIP

--- src/test/kotlin/Year2018/Puzzle15Test.kt
+++ src/test/kotlin/Year2018/Puzzle15Test.kt
@@ -1,12 +1,11 @@
 package Year2018
 
-import junit.framework.Assert
 import junit.framework.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class Puzzle15Test {
-    val puzzleText = this::class.java.getResource(
-            "/2018/puzzle15.txt").readText()
+    val puzzleText = this::class.java.getResource("/2018/puzzle15.txt").readText().replace("\r", "")
     val puzzle = Puzzle15()
 
     @Test
@@ -22,8 +21,172 @@ class Puzzle15Test {
     }
 
     class Puzzle15 {
+        enum class Type { ELF, GOBLIN }
+
+        data class Point(val x: Int, val y: Int) {
+            fun getFreeAdjacentTiles(grid: Map<Point, Char>): List<Point> {
+                val northPoint = this.copy(y = this.y - 1)
+                val southPoint = this.copy(y = this.y + 1)
+                val westPoint = this.copy(x = this.x - 1)
+                val eastPoint = this.copy(x = this.x + 1)
+
+                return listOf(northPoint, southPoint, westPoint, eastPoint)
+                    .map { it to grid[it] }
+                    .filter { it.second != null && it.second == '.' }
+                    .map { it.first }
+            }
+        }
+
+        data class Elf(override val position: Point, override val type: Type = Type.ELF, override val hp: Int = 200, override val attack: Int = 3) : Soldier
+
+        data class Goblin(override val position: Point, override val type: Type = Type.GOBLIN, override val hp: Int = 200, override val attack: Int = 3) : Soldier
+
+        interface Soldier {
+            val type: Type
+            val position: Point
+            val hp: Int
+            val attack: Int
+
+            fun getFreeAdjacentTiles(grid: Map<Point, Char>): List<Point> {
+                return position.getFreeAdjacentTiles(grid)
+            }
+        }
+
+        val unitCompare = Comparator<Soldier> { a, b -> pointCompare.compare(a.position, b.position) }
+
+        val pointCompare = Comparator<Point> { a, b ->
+            when {
+                a.y > b.y -> 1
+                a.y < b.y -> -1
+                a.x > b.x -> 1
+                a.x < b.x -> -1
+                else -> 0
+            }
+        }
+
         fun solveOne(puzzleText: String): String {
+            val grid = parseGrid(puzzleText)
+
+            // Go through the grid, find a list of goblins
+            val units = getUnits(grid)
+
+            for (index in 0 until units.size) {
+                val currentUnit = units[index]
+
+                // 1. identifying all possible targets (enemy units)
+                val enemyUnits = units.filter { it.type != currentUnit.type }
+
+                // 2. identifies all of the open squares (.) that are in range of each target;
+                // these are the squares which are adjacent (immediately up, down, left, or right) to any target and
+                // which aren't already occupied by a wall or another unit.
+                val pointAdjacentToEnemy = enemyUnits.flatMap { enemy -> enemy.getFreeAdjacentTiles(grid) }.toSet()
+
+                // Already next to an enemy, do not need to move
+                if (pointAdjacentToEnemy.contains(currentUnit.position)) {
+
+                }
+                else {
+
+                    val reachablePoints = pointAdjacentToEnemy
+                        .filter { point -> isReachable(grid, currentUnit.position, point) }
+                        .map { point -> point to manhattanDistance(currentUnit.position, point) }
+
+                    val minDistance: Int = reachablePoints.minBy { it.second }!!.second
+
+                    val chosenPoint = reachablePoints
+                        .filter { it.second == minDistance }
+                        .map { it.first }
+                        .sortedWith(pointCompare)
+                        .first()
+
+
+
+
+//                        .minBy { it.second }
+
+
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+                // 3. attack the enemy unit if you are already in range of it.
+
+
+
+            }
+
+
+
+
             return ""
+        }
+
+        private fun isReachable(grid: Map<Point, Char>, start: Point, end: Point): Boolean {
+            if (end == start) return true
+
+            // Start at start
+            val toProcess = LinkedList<Point>()
+            val visited = mutableSetOf<Point>()
+
+            while (toProcess.isNotEmpty()) {
+                val currentPoint = toProcess.pop()
+
+                if (currentPoint == end) {
+                    return true
+                }
+
+                visited.add(currentPoint)
+
+                val adjacentTiles: List<Point> = currentPoint.getFreeAdjacentTiles(grid)
+                    .filter { !visited.contains(it) }
+
+                // Add adjacent tiles we have not visited
+                toProcess.addAll(adjacentTiles)
+            }
+
+            return false
+        }
+
+        private fun getUnits(grid: Map<Point, Char>): List<Soldier> {
+            val units = grid.mapNotNull { (point, char) ->
+                when (char) {
+                    'G' -> Goblin(point)
+                    'E' -> Elf(point)
+                    else -> null
+                }
+            }.sortedWith(unitCompare)
+            return units
+        }
+
+        fun manhattanDistance(a: Point, b: Point): Int {
+            return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+        }
+
+        private fun parseGrid(puzzleText: String): Map<Point, Char> {
+            val lines = puzzleText.split("\n")
+            val width = lines[0].length
+            val height = lines.size
+
+            return (0 until width).flatMap { x ->
+                (0 until height).map { y ->
+
+                    val point = Point(x, y)
+                    val char = lines[y][x]
+
+                    point to char
+                }
+            }.toMap()
         }
 
         fun solveTwo(puzzleText: String): String {
