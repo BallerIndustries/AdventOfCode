a4a47af02e518de67729a685cea859580f7c47e4
anguruso <baller.industries@hotmail.com>
2018-12-22 14:34:07 +0800

    FINALLY DONE!

--- src/test/kotlin/Year2018/Puzzle15Test.kt
+++ src/test/kotlin/Year2018/Puzzle15Test.kt
@@ -24,17 +24,78 @@ class Puzzle15Test {
         """.trimIndent()
 
     @Test
-    @Ignore
-    fun `puzzle part a`() {
-        val result = puzzle.solveOne(puzzleText)
-        assertEquals("a", result)
+    //@Ignore("This case is failling, and I do not care to fix it")
+    fun `summary number 1`() {
+        val initial = """
+            #######
+            #G..#E#
+            #E#E.E#
+            #G.##.#
+            #...#E#
+            #...E.#
+            #######
+            """.trimIndent()
+
+        val expectedState = """
+            #######
+            #...#E#
+            #E#...#
+            #.E##.#
+            #E..#E#
+            #.....#
+            #######
+        """.trimIndent()
+
+        val (score, state) = puzzle.solveOneWithState(initial)
+        assertEquals(expectedState, state)
+        assertEquals(36334, score)
     }
 
     @Test
-    @Ignore
+    fun `summary number 2`() {
+        val initial = """
+            #######
+            #E..EG#
+            #.#G.E#
+            #E.##E#
+            #G..#.#
+            #..E#.#
+            #######
+            """.trimIndent()
+
+        val expectedState = """
+            #######
+            #.E.E.#
+            #.#E..#
+            #E.##.#
+            #.E.#.#
+            #...#.#
+            #######
+        """.trimIndent()
+
+        val (score, state) = puzzle.solveOneWithState(initial)
+        assertEquals(expectedState, state)
+        assertEquals(39514, score)
+    }
+
+    @Test
+    fun `puzzle part a`() {
+        // 208160 is too low
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(208960, result)
+    }
+
+    @Test
+    @Ignore("Runs too slowly")
     fun `puzzle part b`() {
+        // Not 23
+        // Not Attack = 23 48864
+        // Not attack = 23 47337 (roundCount - 1)
+        // Not Attack = 25 48515
+        // Not Attack = 25 46950 (roundCount - 1)
+        // Not Attack = 67 38913
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals("b", result)
+        assertEquals(49863, result)
     }
 
     @Test
@@ -249,37 +310,72 @@ class Puzzle15Test {
     @Test
     fun `example a, goblin 1 should be able to reach elf`() {
         val grid = puzzle.parseGrid(exampleText)
-        val canBeReached = puzzle.isReachable(grid, Puzzle15.Point(1, 1), Puzzle15.Point(3, 4))
+        val ( canBeReached, stepCount ) = puzzle.isReachable(grid, Puzzle15.Point(1, 1), Puzzle15.Point(3, 4))
         assertTrue(canBeReached)
+        assertEquals(5, stepCount.size)
+    }
+
+    @Test
+    fun `example a, goblin 1 should be able to reach point right next to him`() {
+        val grid = puzzle.parseGrid(exampleText)
+        val ( canBeReached, stepCount ) = puzzle.isReachable(grid, Puzzle15.Point(1, 1), Puzzle15.Point(2, 1))
+        assertTrue(canBeReached)
+        assertEquals(1, stepCount.size)
+    }
+
+    @Test
+    fun `example a, goblin 1 should be able to reach his pwn point`() {
+        val grid = puzzle.parseGrid(exampleText)
+        val ( canBeReached, stepCount ) = puzzle.isReachable(grid, Puzzle15.Point(1, 1), Puzzle15.Point(1, 1))
+        assertTrue(canBeReached)
+        assertEquals(0, stepCount.size)
     }
 
     class Puzzle15 {
 
-        fun solveOne(puzzleText: String): Int {
+        val defaultElfGenerator = { point: Point -> Elf(point) }
+
+        fun solveOneWithState(puzzleText: String, elfGenerator: (Point) -> Elf = defaultElfGenerator): Triple<Int, String, Int> {
             var grid = parseGrid(puzzleText)
-            val soldiers = getSoldiers(grid)
+            val soldiers = getSoldiers(grid, elfGenerator)
             var roundCount = 0
 
             while (true) {
-                val elfTotalHp = soldiers.filter { it.type == Type.ELF }.sumBy { it.hp }
-                val goblinTotalHp = soldiers.filter { it.type == Type.GOBLIN }.sumBy { it.hp }
-
-                if (elfTotalHp <= 0 || goblinTotalHp <= 0) {
-                    break
-                }
-
                 grid = runCycle(grid, soldiers)
+
+                if (oneSideIsDead(soldiers)) break
+
+
+
                 roundCount++
 
-                println("Round $roundCount done!")
+                //println("Round $roundCount done! elfTotalHp = $elfTotalHp goblinTotalHp = $goblinTotalHp")
             }
 
-            return soldiers.filter { !it.isDead() }.sumBy { it.hp } * roundCount
+            val score = soldiers.filter { !it.isDead() }.sumBy { it.hp } * (roundCount)
+            val state = outputGrid(grid)
+            val numElfDeaths = soldiers.filter { it.isDead() && it.type == Type.ELF }.count()
+
+            return Triple(score, state, numElfDeaths)
+        }
+
+        private fun oneSideIsDead(soldiers: List<Soldier>): Boolean {
+            val elvesAlive = soldiers.filter { it.type == Type.ELF }.count { !it.isDead() }
+            val goblinsAlive = soldiers.filter { it.type == Type.GOBLIN }.count { !it.isDead() }
+
+            if (elvesAlive == 0 || goblinsAlive == 0) {
+                return true
+            }
+            return false
+        }
+
+        fun solveOne(puzzleText: String): Int {
+            return solveOneWithState(puzzleText).first
         }
 
         fun stateAfter(puzzleText: String, numCycles: Int = 10): String {
             var grid = parseGrid(puzzleText)
-            val soldiers = getSoldiers(grid)
+            val soldiers = getSoldiers(grid) { point -> Elf(point) }
 
             (0 until numCycles).forEach {
                 grid = runCycle(grid, soldiers)
@@ -293,19 +389,35 @@ class Puzzle15Test {
             val sortedSoldiers = soldiers.filter { !it.isDead() }.sortedWith(unitCompare)
 
             for (index in 0 until sortedSoldiers.size) {
+
+                if (oneSideIsDead(soldiers)) {
+                    return mutableGridState
+                }
+
+
                 val currentUnit = sortedSoldiers[index]
 
+                if (currentUnit.isDead()) {
+                    continue
+                }
+
+
                 mutableGridState = tryMoveSoldier(currentUnit, mutableGridState, sortedSoldiers)
-                println("Unit #$index finished move")
+                //println("Unit #$index/${sortedSoldiers.lastIndex} finished move")
 
                 mutableGridState = tryAttackEnemy(currentUnit, mutableGridState, sortedSoldiers)
-                println("Unit #$index finished attack")
+                //println("Unit #$index/${sortedSoldiers.lastIndex} finished attack")
             }
 
             return mutableGridState
         }
 
         private fun tryAttackEnemy(currentUnit: Soldier, grid: Map<Point, Char>, soldiers: List<Soldier>): Map<Point, Char> {
+            if (currentUnit.isDead()) {
+                return grid
+//                println("Hey I am dead and I am trying to attack! Wtf mate")
+            }
+
             val enemyUnits = soldiers.filter { it.type != currentUnit.type && !it.isDead() }
 
             val enemiesNextToMe = currentUnit
@@ -318,12 +430,17 @@ class Puzzle15Test {
 
             // Attack
             val enemyToAttack = enemiesNextToMe.sortedWith(hpAndPointCompare).first()
-            enemyToAttack.receiveDamage(3)
+            enemyToAttack.receiveDamage(currentUnit.attack)
 
             return updateGridState(grid, soldiers)
         }
 
         private fun tryMoveSoldier(currentUnit: Soldier, grid: Map<Point, Char>, soldiers: List<Soldier>): Map<Point, Char> {
+            if (currentUnit.isDead()) {
+                println("I am dead and trying to move wtf mate!")
+            }
+
+
             val enemyUnits = soldiers.filter { it.type != currentUnit.type && !it.isDead() }
             val pointsAdjacentToEnemies = enemyUnits.flatMap { enemy -> enemy.position.getAdjacentTiles() }.toSet()
 
@@ -332,27 +449,40 @@ class Puzzle15Test {
                 return grid
             }
 
-            val reachablePoints = pointsAdjacentToEnemies
-                .filter { point -> isReachable(grid, currentUnit.position, point) }
+            // Find N, E, S, W reachable points
 
-            val pathsToPoints = efficientGetPathsToPoints(reachablePoints, grid, currentUnit)
+            val reachablePointsToPath = pointsAdjacentToEnemies
+                .map { point -> point to isReachable(grid, currentUnit.position, point) }
+                .filter { it.second.first }
+                .map { it.first to it.second.second }
 
-            // Ah shit, there are no paths to this point. No moving
-            if (pathsToPoints.isEmpty()) {
+            // No paths, we cannot move. Job done.
+            if (reachablePointsToPath.isEmpty()) {
                 return grid
             }
 
-            val minPathLength = pathsToPoints.minBy { it.size }!!.size
+            val minPathLength = reachablePointsToPath.minBy { it.second.size }!!.second.size
 
-            val possibleNextSteps = pathsToPoints
-                .filter { it.size == minPathLength }
-                .map { it.first() }
-                .distinct()
+            val chosenPoint = reachablePointsToPath
+                .filter { it.second.size == minPathLength }
+                .map { it.first }
                 .sortedWith(pointCompare)
+                .first()
 
-            // Move the soldier to the chosen point
-            val chosenPoint = possibleNextSteps.first()
-            currentUnit.moveTo(chosenPoint)
+            // Okay we know which point we should go for. Now we need to decide which step to take. NORTH, WEST, EAST or SOUTH
+            val dog = currentUnit.position.getFreeAdjacentTiles(grid)
+                .map { point ->
+                    val (isReachable, path) = isReachable(grid, point, chosenPoint)
+                    val mutable = path.toMutableList()
+                    mutable.add(0, point)
+                    isReachable to mutable
+                }
+                .filter { it.first }
+                .map { it.second }
+
+            val minDogLength = dog.minBy { it.size }!!.size
+            val stepToTake = dog.filter { it.size == minDogLength }.first().first()
+            currentUnit.moveTo(stepToTake)
 
             return updateGridState(grid, soldiers)
         }
@@ -364,10 +494,10 @@ class Puzzle15Test {
             if (pointsSortedByManDistance.isEmpty()) return emptyList()
 
             // Output min man distance
-            println("Minimum distance to point is ${manhattanDistance(currentUnit.position, pointsSortedByManDistance.first())}")
+            //println("Minimum distance to point is ${manhattanDistance(currentUnit.position, pointsSortedByManDistance.first())}")
 
             val shortestPathToFirstPoint = getShortestPaths(grid, currentUnit.position, pointsSortedByManDistance.first())
-            var minPathLength = shortestPathToFirstPoint.first().size
+            var minPathLength = shortestPathToFirstPoint!!.first().size
 
             val allPaths = pointsSortedByManDistance.mapNotNull { point ->
 
@@ -379,11 +509,15 @@ class Puzzle15Test {
                 else {
                     val jurPaths = getShortestPaths(grid, currentUnit.position, point, minPathLength)
 
-                    if (jurPaths.firstOrNull()?.size ?: Int.MAX_VALUE < minPathLength) {
-                        minPathLength = jurPaths.first().size
+                    if (jurPaths == null) {
+                        jurPaths
                     }
-
-                    jurPaths
+                    else {
+                        if (jurPaths!!.firstOrNull()?.size ?: Int.MAX_VALUE < minPathLength) {
+                            minPathLength = jurPaths.first().size
+                        }
+                        jurPaths
+                    }
                 }
 
             }.flatten()
@@ -410,7 +544,7 @@ class Puzzle15Test {
         }
 
 
-        fun getShortestPaths(grid: Map<Point, Char>, a: Point, b: Point, maxLength: Int = Int.MAX_VALUE): List<List<Point>> {
+        fun getShortestPaths(grid: Map<Point, Char>, a: Point, b: Point, maxLength: Int = Int.MAX_VALUE): List<List<Point>>? {
             var allPaths = listOf(listOf(a))
 
 
@@ -423,8 +557,10 @@ class Puzzle15Test {
                     break
                 }
 
+                if (allPaths.any { it.size > maxLength }) return null
+
                 allPaths = allPaths.flatMap { list ->
-                    val lastItem = list.last()!!
+                    val lastItem = list.last()
                     val nextTiles = lastItem.getFreeAdjacentTiles(grid).filter { !list.contains(it) }
                     nextTiles.map { nextTile -> list + nextTile }
                 }
@@ -441,6 +577,11 @@ class Puzzle15Test {
         enum class Type(val char: Char) { ELF('E'), GOBLIN('G') }
 
         data class Point(val x: Int, val y: Int) {
+            fun north() = this.copy(y = this.y - 1)
+            fun south() = this.copy(y = this.y + 1)
+            fun west() = this.copy(x = this.x - 1)
+            fun east() = this.copy(x = this.x + 1)
+
             fun getFreeAdjacentTiles(grid: Map<Point, Char>): List<Point> {
                 return getAdjacentTiles()
                     .map { it to grid[it] }
@@ -449,12 +590,7 @@ class Puzzle15Test {
             }
 
             fun getAdjacentTiles(): List<Point> {
-                val northPoint = this.copy(y = this.y - 1)
-                val southPoint = this.copy(y = this.y + 1)
-                val westPoint = this.copy(x = this.x - 1)
-                val eastPoint = this.copy(x = this.x + 1)
-
-                return listOf(northPoint, southPoint, westPoint, eastPoint)
+                return listOf(north(), west(), east(), south())
             }
         }
 
@@ -526,37 +662,39 @@ class Puzzle15Test {
             return dog
         }
 
-        fun isReachable(grid: Map<Point, Char>, start: Point, end: Point): Boolean {
+        fun isReachable(grid: Map<Point, Char>, start: Point, end: Point): Pair<Boolean, List<Point>> {
             val visited = mutableSetOf(start)
-            val toProcess = LinkedList<Point>()
-            toProcess.add(start)
+            val toProcess = LinkedList<Pair<Point, MutableList<Point>>>()
+            toProcess.add(start to mutableListOf())
 
             while (toProcess.isNotEmpty()) {
-                val currentPoint = toProcess.pop()
+                val (currentPoint, path) = toProcess.pop()
 
                 if (currentPoint == end) {
-                    return true
+                    return true to path
                 }
 
                 visited.add(currentPoint)
 
-                val adjacentTiles: List<Point> = currentPoint
+                val adjacentTiles = currentPoint
                     .getFreeAdjacentTiles(grid)
                     .filter { !visited.contains(it) }
+                    .map { it to (path + it).toMutableList() }
 
                 // Add adjacent tiles we have not visited
                 toProcess.addAll(adjacentTiles)
-                visited.addAll(adjacentTiles)
+                visited.addAll(adjacentTiles.map { it.first })
+//                stepCount++
             }
 
-            return false
+            return false to listOf()
         }
 
-        private fun getSoldiers(grid: Map<Point, Char>): List<Soldier> {
+        private fun getSoldiers(grid: Map<Point, Char>, elfGenerator: (Point) -> Elf): List<Soldier> {
             val units = grid.mapNotNull { (point, char) ->
                 when (char) {
                     'G' -> Goblin(point)
-                    'E' -> Elf(point)
+                    'E' -> elfGenerator(point)
                     else -> null
                 }
             }.sortedWith(unitCompare)
@@ -579,8 +717,25 @@ class Puzzle15Test {
             }.toMap()
         }
 
-        fun solveTwo(puzzleText: String): String {
-            return ""
+        fun solveTwo(puzzleText: String): Int {
+            var currentAttack = 68
+            var lastSuccessfulScore = 0
+
+            while (true) {
+                val elfGenerator = { point: Point -> Elf(point, attack = currentAttack)}
+                val (score, _, numDeadElves) = solveOneWithState(puzzleText, elfGenerator)
+
+                if (numDeadElves > 0) {
+                    println("An elf died when currentAttack = $currentAttack")
+                    return lastSuccessfulScore
+                }
+                else {
+                    lastSuccessfulScore = score
+                }
+
+                println("Flawless elf victory when currentAttack = $currentAttack score = $score. Going to try currentAttack = ${currentAttack - 1}")
+                currentAttack--
+            }
         }
 
 
