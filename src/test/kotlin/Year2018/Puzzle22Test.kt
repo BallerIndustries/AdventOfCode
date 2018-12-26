10a3004cd993c9db995cf23898575cf8ae94452e
anguruso <baller.industries@hotmail.com>
2018-12-26 23:39:55 +0800

    correct, but possibly too slow

--- src/test/kotlin/Year2018/Puzzle22Test.kt
+++ src/test/kotlin/Year2018/Puzzle22Test.kt
@@ -2,11 +2,21 @@ package Year2018
 
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import java.io.File
 
 class Puzzle22Test {
     val puzzleText = this::class.java.getResource("/2018/puzzle22.txt").readText().replace("\r", "")
     val puzzle = Puzzle22()
+
+    @Test
+    fun `example 1`() {
+        val text = """
+            depth: 510
+            target: 10,10
+        """.trimIndent()
+
+        val result = puzzle.solveOne(text)
+        assertEquals(114, result)
+    }
 
     @Test
     fun `puzzle part a`() {
@@ -22,31 +32,44 @@ class Puzzle22Test {
 
     class Puzzle22 {
 
-        enum class RegionType { ROCKY, WET, NARROW }
+        enum class RegionType(val riskLevel: Int) { ROCKY(0), WET(1), NARROW(2) }
 
         data class Point(val x: Int, val y: Int) {
             companion object {
+                val ZERO = Point(0, 0)
+
                 fun from(text: String): Point {
                     return text.split(",").map { it.toInt() }.let { Point(it[0], it[1]) }
                 }
             }
 
-            fun calculateGeologicIndex(depth: Int): Int {
+            fun calculateGeologicIndex(target: Point, depth: Int): Int {
                 return when {
-                    x == 0 && y == 0 -> 0
-                    x == 10 && y == 10 -> 0
+                    this == ZERO -> 0
+                    this == target -> 0
                     y == 0 -> x * 16807
                     x == 0 -> y * 48271
                     else -> {
                         val a = this.copy(x = x - 1)
                         val b = this.copy(y = y - 1)
-                        a.erosionLevel(depth) * b.erosionLevel(depth)
+                        a.calculateErosionLevel(target, depth) * b.calculateErosionLevel(target, depth)
                     }
                 }
             }
 
-            private fun erosionLevel(depth: Int): Int {
-                return this.calculateGeologicIndex(depth) + depth
+            private fun calculateErosionLevel(target: Point, depth: Int): Int {
+                return (this.calculateGeologicIndex(target, depth) + depth) % 20183
+            }
+
+            fun calculateRegionType(target: Point, depth: Int): RegionType {
+                val horse = calculateErosionLevel(target, depth) % 3
+
+                return when (horse) {
+                    0 -> RegionType.ROCKY
+                    1 -> RegionType.WET
+                    2 -> RegionType.NARROW
+                    else -> throw RuntimeException("horse should be between 0 and 2 weird! horse = $horse")
+                }
             }
         }
 
@@ -58,7 +81,13 @@ class Puzzle22Test {
 
             println("depth = $depth target = $target")
 
-            return 38943
+            val regionTypes = (0 .. target.y).flatMap { y ->
+                (0 .. target.x).map { x ->
+                    Point(x, y).calculateRegionType(target, depth)
+                }
+            }
+
+            return regionTypes.sumBy { it.riskLevel }
         }
 
 
