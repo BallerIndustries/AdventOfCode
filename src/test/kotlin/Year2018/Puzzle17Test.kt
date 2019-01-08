b22ff92dc3a94a53ca1cd9494804a7b434d4dcc0
Angus Cheng <anguscheng5@gmail.com>
2019-01-09 02:03:40 +0800

    Very nice job Angus, Day 17 finally done

--- src/test/kotlin/Year2018/Puzzle17Test.kt
+++ src/test/kotlin/Year2018/Puzzle17Test.kt
@@ -21,19 +21,26 @@ class Puzzle17Test {
     @Test
     fun `render puzzle part a`() {
         val result = puzzle.renderInitial(puzzleText)
-        assertEquals("a", result)
+        println(result)
     }
 
     @Test
     fun `puzzle example a`() {
-        val result = puzzle.solveOneFillzors(exampleText)
+        val (result, _) = puzzle.solveOneFillzors(exampleText)
         assertEquals(57, result)
     }
 
     @Test
     fun `puzzle part a`() {
-        val result = puzzle.solveOneFillzors(puzzleText)
-        assertEquals(9000, result)
+        // 36789 too high
+        val (result, _) = puzzle.solveOneFillzors(puzzleText)
+        assertEquals(36787, result)
+    }
+
+    @Test
+    fun `puzzle part b`() {
+        val (_, result) = puzzle.solveOneFillzors(puzzleText)
+        assertEquals(29662, result)
     }
 
     enum class Collision { WALL, GAP }
@@ -41,61 +48,68 @@ class Puzzle17Test {
     class Puzzle17 {
         private val movingWaterPoints = mutableSetOf<Point>()
 
-        fun solveOneFillzors(puzzleText: String): Int {
+        fun solveOneFillzors(puzzleText: String): Pair<Int, Int> {
             val state = createInitialState(puzzleText)
             val stateWithWater = state.toMutableMap()
 
-            // TODO: Lets actually deal with these please
-            val otherStreamsToDealWith = mutableListOf<Point>()
-            var currentPoint = Point(500, 0)
+            val streamsYo = mutableListOf(Point(500, 0))
+            var index = 0
 
-            val maxY = state.keys.maxBy { it.y }!!.y
+            while (index < streamsYo.size) {
+                var currentPoint = streamsYo[index]
 
-            // While we are still within the map
-            while (true) {
-                println("currentPoint = $currentPoint")
-                println(maxY)
+                // While we are still within the map
+                while (true) {
+                    currentPoint = shootDownUntilYouHitGround(state, currentPoint) ?: break
+                    var (leftPoint, leftCollision) = chargeLeftUntilWallOrGap(state, currentPoint)
+                    var (rightPoint, rightCollision) = chargeRightUntilWallOrGap(state, currentPoint)
 
-                currentPoint = shootDownUntilYouHitGround(state, currentPoint) ?: break
-                var (leftPoint, leftCollision) = chargeLeftUntilWallOrGap(state, currentPoint)
-                var (rightPoint, rightCollision) = chargeRightUntilWallOrGap(state, currentPoint)
+                    // We are in a bucket
+                    if (leftCollision == Collision.WALL && rightCollision == Collision.WALL) {
+                        currentPoint = handleBucket(currentPoint, leftPoint, rightPoint, stateWithWater)
 
-                // We are in a bucket
-                if (leftCollision == Collision.WALL && rightCollision == Collision.WALL) {
-                    currentPoint = handleBucket(currentPoint, leftPoint, rightPoint, stateWithWater)
+                        // Recalc the jerks
+                        val (_leftPoint, _leftCollision) = chargeLeftUntilWallOrGap(stateWithWater, currentPoint)
+                        val (_rightPoint, _rightCollision) = chargeRightUntilWallOrGap(stateWithWater, currentPoint)
 
-                    //println(renderState(stateWithWater))
+                        leftPoint = _leftPoint
+                        leftCollision = _leftCollision
+                        rightPoint = _rightPoint
+                        rightCollision = _rightCollision
+                    }
 
-                    // Recalc the jerks
-                    val (_leftPoint, _leftCollision) = chargeLeftUntilWallOrGap(stateWithWater, currentPoint)
-                    val (_rightPoint, _rightCollision) = chargeRightUntilWallOrGap(stateWithWater, currentPoint)
+                    // We should be able to charge left/right and drop off
+                    if (leftCollision == Collision.GAP && rightCollision == Collision.GAP) {
+                        setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
 
-                    leftPoint = _leftPoint
-                    leftCollision = _leftCollision
-                    rightPoint = _rightPoint
-                    rightCollision = _rightCollision
+                        if (!streamsYo.contains(rightPoint)) {
+                            streamsYo.add(rightPoint)
+                        }
+
+                        currentPoint = leftPoint
+                    }
+                    else if (leftCollision == Collision.GAP && rightCollision == Collision.WALL) {
+                        setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
+                        currentPoint = leftPoint
+                    }
+                    else if (leftCollision == Collision.WALL && rightCollision == Collision.GAP) {
+                        setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
+                        currentPoint = rightPoint
+                    }
                 }
 
-                // We should be able to charge left/right and drop off
-                if (leftCollision == Collision.GAP && rightCollision == Collision.GAP) {
-                    setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
-                    otherStreamsToDealWith.add(rightPoint)
-                    currentPoint = leftPoint
-                }
-                else if (leftCollision == Collision.GAP && rightCollision == Collision.WALL) {
-                    setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
-                    currentPoint = leftPoint
-                }
-                else if (leftCollision == Collision.WALL && rightCollision == Collision.GAP) {
-                    setHorizontalLine(stateWithWater, leftPoint, rightPoint, '|')
-                    currentPoint = rightPoint
-                }
+                index++
             }
 
-            movingWaterPoints.forEach { point -> if (!stateWithWater.containsKey(point)) stateWithWater[point] = '|' }
-            println(renderState(stateWithWater))
+            val minY = state.entries.filter  { it.value != '+' }.minBy { it.key.y }!!.key.y
+            val maxY = state.keys.maxBy { it.y }!!.y
 
-            return stateWithWater.values.count { it == '~' || it == '|' }
+            movingWaterPoints.forEach { point -> if (!stateWithWater.containsKey(point)) stateWithWater[point] = '|' }
+
+            val waterCount = stateWithWater.entries.filter { it.key.y >= minY && it.key.y <= maxY }.count { it.value == '~' || it.value == '|' }
+            val retainedWaterCount = stateWithWater.entries.filter { it.key.y >= minY && it.key.y <= maxY }.count { it.value == '~' }
+
+            return waterCount to retainedWaterCount
         }
 
         private fun handleBucket(startingPoint: Point, firstLeftPoint: Point, firstRightPoint: Point, stateWithWater: MutableMap<Point, Char>): Point {
@@ -176,246 +190,6 @@ class Puzzle17Test {
             }
 
             return if (currentPosition.y >= maxY) null else currentPosition
-        }
-
-//        fun solveOneSpeedy(puzzleText: String): Int {
-//            val state = createInitialState(puzzleText)
-//            val waterSource = Point(500, 0)
-//            val waterMolecules = mutableSetOf<Point>()
-//
-//            while (true) {
-//                // Create a water molecule
-//                var molecule = waterSource.down()
-//                var direction = WaterDirection.DOWN
-//
-//                // Move the jerk down until it hits a something
-//                while (true) {
-//                    if (waterMolecules.count() == 15) {
-//                        println("Hooray")
-//                    }
-//
-//                    if (direction == WaterDirection.DOWN) {
-//                        molecule = goDownUntilColliding(molecule, state, waterMolecules)
-//                    }
-//
-//                    // Okay we are done falling, should we go left or right?
-//                    if (isFree(molecule.left(), state, waterMolecules)) {
-//                        direction = WaterDirection.LEFT
-//                    }
-//                    else if (isFree(molecule.right(), state, waterMolecules)) {
-//                        direction = WaterDirection.RIGHT
-//                    }
-//                    else {
-//                        break
-//                    }
-//
-//                    val tmp: Pair<Point, WaterDirection> = moveHorizontally(direction, molecule, state, waterMolecules)
-//
-//                    molecule = tmp.first
-//                    direction = tmp.second
-//
-//                    if (direction == WaterDirection.STUCK) {
-//                        break
-//                    }
-//                }
-//
-//                waterMolecules.add(molecule)
-//                //println(waterMolecules.size)
-//
-//                println(movingWaterPoints.size)
-//
-//                if (waterMolecules.count() % 2000 == 0) {
-//                    println(renderState(state, waterMolecules))
-//                }
-//            }
-//        }
-
-//        private fun moveHorizontally(direction: WaterDirection, molecule: Point, state: Map<Point, Char>, waterMolecules: Set<Point>): Pair<Point, WaterDirection> {
-//            var mutableMolecule = molecule
-//            movingWaterPoints.add(mutableMolecule)
-//
-//            if (direction == WaterDirection.LEFT) {
-//
-//                while (isFree(mutableMolecule.left(), state, waterMolecules)) {
-//
-//                    // Wait a minute! We should be fallllinnnnggg!!
-//                    if (isFree(mutableMolecule.down(), state, waterMolecules)) {
-//                        return mutableMolecule to WaterDirection.DOWN
-//                    }
-//
-//                    mutableMolecule = mutableMolecule.left()
-//                    movingWaterPoints.add(mutableMolecule)
-//                }
-//
-//                return mutableMolecule to WaterDirection.STUCK
-//            }
-//            else if (direction == WaterDirection.RIGHT) {
-//
-//
-//                while (isFree(mutableMolecule.right(), state, waterMolecules)) {
-//
-//                    // Wait a minute! We should be fallllinnnnggg!!
-//                    if (isFree(mutableMolecule.down(), state, waterMolecules)) {
-//                        return mutableMolecule to WaterDirection.DOWN
-//                    }
-//
-//                    mutableMolecule = mutableMolecule.right()
-//                    movingWaterPoints.add(mutableMolecule)
-//                }
-//
-//                return mutableMolecule to WaterDirection.STUCK
-//            }
-//            else {
-//                throw RuntimeException("Oh no")
-//            }
-//        }
-//
-//        enum class WaterDirection { STUCK, DOWN, LEFT, RIGHT }
-//
-//        private fun goDownUntilColliding(molecule: Point, state: Map<Point, Char>, waterMolecules: Set<Point>): Point {
-//            var molecule1 = molecule
-//            movingWaterPoints.add(molecule1)
-//
-//            while (isFree(molecule1.down(), state, waterMolecules)) {
-//                molecule1 = molecule1.down()
-//                movingWaterPoints.add(molecule1)
-//            }
-//
-//            return molecule1
-//        }
-
-//        fun goLeftOrRight(molecule: Point, state: Map<Point, Char>, waterMolecules: List<Point>): Point {
-//            var mutableMolecule = molecule
-//            val firstLeftIsFree = isFree(mutableMolecule.left(), state, waterMolecules)
-//
-//            if (firstLeftIsFree) {
-//                while (isFree(mutableMolecule.left(), state, waterMolecules)) {
-//                    mutableMolecule = mutableMolecule.left()
-//                }
-//            }
-//            else {
-//                while (isFree(mutableMolecule.right(), state, waterMolecules)) {
-//                    mutableMolecule = mutableMolecule.right()
-//                }
-//            }
-//
-//            return mutableMolecule
-//        }
-
-//        fun solveOne(puzzleText: String): Int {
-//            val state = createInitialState(puzzleText)
-//            val waterSource = Point(500, 0)
-//            var waterSquares = mutableListOf<Point>()
-//
-//            val outputFile = File("/Users/anguruso/Desktop/jur.txt")
-//
-//            if (outputFile.exists()) {
-//                outputFile.delete()
-//            }
-//
-//            while (true) {
-//                // Move all the other waters
-//                val tmp = moveTheWaterAlong(state, waterSquares)
-//                val noWaterMoleculesMoved = tmp.first
-//
-//                if (!noWaterMoleculesMoved) {
-//                    break
-//                }
-//
-//                waterSquares = tmp.second.toMutableList()
-//
-//                // Generate a square at one point below the water source
-//                waterSquares.add(waterSource.down())
-//
-//                //outputFile.appendText(waterSquares.count().toString())
-//                println(waterSquares.size)
-//
-//                if (waterSquares.count() % 300 == 0) {
-//                    outputFile.appendText(renderState(state, waterSquares))
-//                    outputFile.appendText("\n\n\n")
-//                }
-//            }
-//
-//            return 1337
-//        }
-
-//        // returns a boolean representing whether all water has moved, and a list of the new water state.
-//        private fun moveTheWaterAlong(state: Map<Point, Char>, waterSquares: Set<Point>): Pair<Boolean, List<Point>> {
-//            if (waterSquares.isEmpty()) {
-//                return true to emptyList()
-//            }
-//
-//            val newWaterSquares = waterSquares.map { it }.toMutableList()
-//            var moveCount = 0
-//
-//            for (index in 0 until newWaterSquares.size) {
-//                val water = newWaterSquares[index]
-//
-//                // Cannot move left/right if any water below is on the maxY
-//                //val streamBelowIsOnMaxY: Boolean = streamBelowIsOnMaxY(water, state, newWaterSquares)
-//                val streamBelowIsOnMaxY = false
-//
-//
-//                // Is the point below free?
-//                if (isFree(water.down(), state, newWaterSquares)) {
-//                    newWaterSquares[index] = water.down()
-//                    moveCount++
-//                }
-//                else if (isFree(water.left(), state, newWaterSquares) && !isOnMaxY(water, state) && !streamBelowIsOnMaxY) {
-//                    newWaterSquares[index] = water.left()
-//                    moveCount++
-//                }
-//                else if (isFree(water.right(), state, newWaterSquares) && !isOnMaxY(water, state) && !streamBelowIsOnMaxY) {
-//                    newWaterSquares[index] = water.right()
-//                    moveCount++
-////                }
-////            }
-//
-//            if (moveCount == 0) {
-//                return false to emptyList()
-//            }
-//            else {
-//                return true to newWaterSquares
-//            }
-//        }
-
-//        private fun streamBelowIsOnMaxY(water: Point, state: Map<Point, Char>, newWaterSquares: MutableList<Point>): Boolean {
-//            val maxY = state.keys.maxBy { it.y }!!.y
-//            var below = water.down()
-//
-//            while (newWaterSquares.contains(below)) {
-//                if (below.y == maxY) {
-//                    return true
-//                }
-//
-//                below = below.down()
-//            }
-//
-//            return false
-//        }
-//
-//        private fun isOnMaxY(point: Point, state: Map<Point, Char>): Boolean {
-//            val maxY = state.keys.maxBy { it.y }!!.y
-//            return point.y == maxY
-//        }
-//
-//        private fun isWithinMaxY(point: Point, state: Map<Point, Char>): Boolean {
-//            val maxY = state.keys.maxBy { it.y }!!.y
-//            return point.y <= maxY
-//        }
-
-//        private fun isFree(point: Point, state: Map<Point, Char>, waterSquares: Set<Point>): Boolean {
-//            val maxY = state.keys.maxBy { it.y }!!.y
-//            val pointHasNoClay = state[point] != '#'
-//            val pointHasNoWater = !waterSquares.contains(point)
-//            val pointIsBelowMaxY = point.y <= maxY
-//
-//            return pointHasNoClay && pointHasNoWater && pointIsBelowMaxY
-//        }
-
-
-        fun solveTwo(puzzleText: String): String {
-            return ""
         }
 
         fun renderInitial(puzzleText: String): String {
