ca154c1d15b828d0b73ff3d6d5ab387c7262ecb6
Angus Cheng <anguscheng5@gmail.com>
2019-01-19 15:00:29 +0800

    not working, need some debugging

--- src/test/kotlin/Year2017/Puzzle22Test.kt
+++ src/test/kotlin/Year2017/Puzzle22Test.kt
@@ -2,16 +2,34 @@ package Year2017
 
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import java.lang.IndexOutOfBoundsException
 
 class Puzzle22Test {
     val puzzle = Puzzle22()
     val puzzleText = this::class.java.getResource("/2017/puzzle22.txt").readText().replace("\r", "")
 
+    val exampleText = """
+            ..#
+            #..
+            ...
+        """.trimIndent()
+
+    @Test
+    fun `example part a`() {
+        val result = puzzle.solveOne(exampleText)
+        assertEquals(5587, result)
+    }
+
+    @Test
+    fun `example part a 70 iterations`() {
+        val result = puzzle.solveOne(exampleText, 70)
+        assertEquals(41, result)
+    }
+
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(6722, result)
+        // 4999 to low
+        assertEquals(0, result)
     }
 
     @Test
@@ -22,40 +40,66 @@ class Puzzle22Test {
 }
 
 class Puzzle22 {
-    data class Point(val x: Int, val y: Int)
+    enum class Direction { UP, DOWN, LEFT, RIGHT;
 
-    fun solveOne(puzzleText: String): Int {
+        fun left(): Direction {
+            return when(this) {
+                UP -> LEFT
+                DOWN -> RIGHT
+                LEFT -> DOWN
+                RIGHT -> UP
+            }
+        }
+
+        fun right(): Direction {
+            return when(this) {
+                UP -> RIGHT
+                DOWN -> LEFT
+                LEFT -> UP
+                RIGHT -> DOWN
+            }
+        }
+    }
+
+    data class Point(val x: Int, val y: Int) {
+        fun move(direction: Direction): Point {
+            return when (direction) {
+                Direction.UP -> this.copy(y - 1)
+                Direction.DOWN -> this.copy(y + 1)
+                Direction.LEFT -> this.copy(x - 1)
+                Direction.RIGHT -> this.copy(x + 1)
+            }
+        }
+    }
+
+    fun solveOne(puzzleText: String, iterations: Int = 10000): Int {
         val grid = parseGrid(puzzleText).toMutableMap()
-        println(grid)
-
         val width = grid.keys.maxBy { it.x }!!.x
         val height = grid.keys.maxBy { it.y }!!.y
         
         var currentPoint = Point(width / 2, height / 2)
         var direction = Direction.UP
-        
-        (0 until 10000).forEach {
-            
+        var infectionsCaused = 0
+
+        (0 until iterations).forEach {
+            direction = if (grid[currentPoint] == '#') direction.right() else direction.left()
+
             // toggle whether char is or is not infected
-            
-            if (grid[currentPoint] == '#') {
-                direction =direction.right()
-                
+            val currentPointData = grid[currentPoint]
+            val newPointData = if (currentPointData == '#') null else '#'
+
+            if (newPointData == '#') {
+                infectionsCaused++
             }
-            else {
-                direction = direction.left()
-                
-            }
-            
+
+            grid[currentPoint] = newPointData
             currentPoint = currentPoint.move(direction)
-            
         }
 
-
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return infectionsCaused
     }
 
-    private fun parseGrid(puzzleText: String): Map<Point, Char> {
+    private fun parseGrid(puzzleText: String): Map<Point, Char?> {
         val lines = puzzleText.split("\n")
         val height = lines.count()
         val width = lines.first().count()
@@ -66,7 +110,7 @@ class Puzzle22 {
                 val p = Point(x, y)
                 val c = lines[y][x]
 
-                p to c
+                if (c == '#') p to c else p to null
             }
         }.toMap()
         return grid
