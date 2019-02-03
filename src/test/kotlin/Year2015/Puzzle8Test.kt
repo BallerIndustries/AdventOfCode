86e19a773957844f6733f42eaf4811b8cfbb871d
Angus Cheng <baller.industries@hotmail.com>
2019-02-03 13:42:11 +0800

    day 8 2015 - harder than I thought it was

--- src/test/kotlin/Year2015/Puzzle8Test.kt
+++ src/test/kotlin/Year2015/Puzzle8Test.kt
@@ -11,13 +11,13 @@ class Puzzle8Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(589999, result)
+        assertEquals(1371, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(18838115, result)
+        assertEquals(2117, result)
     }
 
     @Test
@@ -34,41 +34,82 @@ class Puzzle8Test {
     fun `length of aaa slash aaa`() {
         assertEquals(7, puzzle.lengthWithoutSpecial("\"aaa\\\"aaa\""))
     }
+
+    @Test
+    fun `length of something crazy`() {
+        assertEquals(1, puzzle.lengthWithoutSpecial("\"\\x27\""))
+    }
 }
 
 class Puzzle8 {
 
     fun solveOne(puzzleText: String): Int {
-        puzzleText.split("\n").forEach { line ->
-
+        return puzzleText.split("\n").sumBy { line ->
             val originalLength = line.length
-            val jur = lengthWithoutSpecial(line)
+            val lengthWithoutSpecial = lengthWithoutSpecial(line)
 
-
-
-
-
-
-
-
+            originalLength - lengthWithoutSpecial
         }
-
-        return 0
     }
 
     fun lengthWithoutSpecial(line: String): Int {
         val stripped = line.substring(1, line.length - 1)
+
+        val dog = stripped.replace("\\\"", "\"")
+                .replace("\\\\", "\\")
+
+
+        return replaceHexNotation(dog).length
+    }
+
+    fun encode(line: String): String {
         var buffer = ""
 
-        var index = 1
+        line.forEach { char ->
+            when (char) {
+                '"' -> buffer += "\\\""
+                '\\' -> buffer += "\\\\"
+                else -> buffer += char
+            }
+        }
 
+        return "\"$buffer\""
+    }
 
-        return stripped.replace("\\\"", "\"")
-                .replace("\\\\", "\\")
-                .length
+    fun replaceHexNotation(text: String): String {
+        val marker = text.map { true }.toMutableList()
+        var i = 3
+
+        while ( i < text.length) {
+            if (text[i-3] == '\\' && text[i-2] == 'x' && isHex(text[i-1]) && isHex(text[i])) {
+                marker[i-3] = false
+                marker[i-2] = true
+                marker[i-1] = false
+                marker[i] = false
+                i += 4
+            }
+            else {
+                i++
+            }
+        }
+
+        val dog = text.filterIndexed { index, char ->
+            marker[index]
+        }
+
+        return dog
+    }
+
+    private fun isHex(c: Char): Boolean {
+        return ('a'..'f').contains(c) || ('0'..'9').contains(c)
     }
 
     fun solveTwo(puzzleText: String): Int {
-       return 0
+        return puzzleText.split("\n").sumBy { line ->
+            val originalLength = line.length
+            val encodedLength = encode(line).length
+
+            encodedLength - originalLength
+        }
     }
 }
