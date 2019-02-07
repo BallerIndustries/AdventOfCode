ac082a4eed607dac4d3d6bdadb0fe0fd77e84868
Angus Cheng <baller.industries@hotmail.com>
2019-02-07 13:31:33 +0800

    WIP

--- src/test/kotlin/Year2015/Puzzle11Test.kt
+++ src/test/kotlin/Year2015/Puzzle11Test.kt
@@ -22,6 +22,54 @@ class Puzzle11Test {
 }
 
 class Puzzle11 {
+    fun hasThreeCharStraight(text: String): Boolean {
+        (2 until text.length).forEach { index ->
+            val c1 = text[index-2]
+            val c2 = text[index-1]
+            val c3 = text[index]
+
+            if ((c2 - c1 == 1) && (c3 - c2 == 1)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun hasNoIOOrL(text: String) = !text.contains('i') && !text.contains('o') && !text.contains('l')
+
+    fun hasTwoNonOverlappingPairs(text: String): Boolean {
+        val pairs = mutableSetOf<String>()
+
+        (1 until text.length).forEach { index ->
+            val c1 = text[index-1]
+            val c2 = text[index]
+
+            if (c1 == c2) pairs.add("$c1$c2")
+            if (pairs.size == 2) return true
+        }
+
+        return false
+    }
+
+    fun passwordToInt(password: String): Int {
+        var multiplier = 1
+        var buffer = 0
+
+        (password.lastIndex .. 0).forEach { index ->
+            val char = password[index]
+            val cheese = (char.toInt() - 97) * multiplier
+            buffer += cheese
+            multiplier *= 26
+        }
+
+        return buffer
+    }
+
+    fun intToPassword(number: Int): String {
+        var multiplier = 1
+    }
+
     fun solveOne(puzzleText: String): Int {
         return 1281928
     }
