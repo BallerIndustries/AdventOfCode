c40f6cd46d6a908a3e625ca4d36b9e19ed797679
Angus Cheng <baller.industries@hotmail.com>
2019-02-09 23:57:14 +0800

    puzzle 15, brute forced

--- src/test/kotlin/Year2015/Puzzle15Test.kt
+++ src/test/kotlin/Year2015/Puzzle15Test.kt
@@ -12,20 +12,77 @@ class Puzzle15Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(2660, result)
+        assertEquals(18965440, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(1256, result)
+        assertEquals(15862900, result)
     }
 }
 
 class Puzzle15 {
     data class Ingredient(val name: String, val capacity: Int, val durability: Int, val flavor: Int, val texture: Int, val calories: Int)
 
+    data class Cookie(val frostingCount: Int, val candyCount: Int, val butterscotchCount: Int, val sugarCount: Int) {
+
+        fun score(ingredients: List<Ingredient>): Int {
+            val frostingIngredient = ingredients.find { it.name == "Frosting" }!!
+            val candyIngredient = ingredients.find { it.name == "Candy" }!!
+            val butterscotchIngredient = ingredients.find { it.name == "Butterscotch" }!!
+            val sugarIngredient = ingredients.find { it.name == "Sugar" }!!
+
+            val capacityScore = (frostingIngredient.capacity * frostingCount) + (candyIngredient.capacity * candyCount) + (butterscotchIngredient.capacity * butterscotchCount) + (sugarIngredient.capacity * sugarCount)
+
+            val durabilityScore = (frostingIngredient.durability * frostingCount) + (candyIngredient.durability * candyCount) + (butterscotchIngredient.durability * butterscotchCount) + (sugarIngredient.durability * sugarCount)
+
+            val flavorScore = (frostingIngredient.flavor * frostingCount) + (candyIngredient.flavor * candyCount) + (butterscotchIngredient.flavor * butterscotchCount) + (sugarIngredient.flavor * sugarCount)
+
+            val textureScore = (frostingIngredient.texture * frostingCount) + (candyIngredient.texture * candyCount) + (butterscotchIngredient.texture * butterscotchCount) + (sugarIngredient.texture * sugarCount)
+
+            val scores = listOf(capacityScore, durabilityScore, flavorScore, textureScore)
+
+            if (scores.any { it < 0 }) {
+                return 0
+            }
+
+            return scores.reduce { acc, score ->  acc * score }
+        }
+
+        fun calories(ingredients: List<Ingredient>): Int {
+            val frostingIngredient = ingredients.find { it.name == "Frosting" }!!
+            val candyIngredient = ingredients.find { it.name == "Candy" }!!
+            val butterscotchIngredient = ingredients.find { it.name == "Butterscotch" }!!
+            val sugarIngredient = ingredients.find { it.name == "Sugar" }!!
+
+            val calories = (frostingIngredient.calories * frostingCount) + (candyIngredient.calories * candyCount) + (butterscotchIngredient.calories * butterscotchCount) + (sugarIngredient.calories * sugarCount)
+            return calories
+        }
+    }
+
     fun solveOne(puzzleText: String): Int {
+        val ingredients = parseInput(puzzleText)
+        val cookies = generateAllCookies()
+        return cookies.map { it.score(ingredients) }.max()!!
+    }
+
+    private fun generateAllCookies(): MutableList<Cookie> {
+        val cookies = mutableListOf<Cookie>()
+
+        (0..100).forEach { frosting ->
+            (0..100 - frosting).forEach { candy ->
+                (0..100 - frosting - candy).forEach { butterscotch ->
+
+                    val sugar = Math.max(100 - frosting - candy - butterscotch, 0)
+                    cookies.add(Cookie(frosting, candy, butterscotch, sugar))
+                }
+            }
+        }
+        return cookies
+    }
+
+    private fun parseInput(puzzleText: String): List<Ingredient> {
         val ingredients = puzzleText.split("\n").map { line ->
 
             val tmp = line.split(" ")
@@ -38,13 +95,13 @@ class Puzzle15 {
 
             Ingredient(name, capacity, durability, flavor, texture, calories)
         }
-
-
-        println(ingredients)
-        return 100
+        return ingredients
     }
 
     fun solveTwo(puzzleText: String): Int {
-        return 400
+        val ingredients = parseInput(puzzleText)
+        val cookies = generateAllCookies()
+
+        return cookies.filter { it.calories(ingredients) == 500 }.map { it.score(ingredients) }.max()!!
     }
 }
