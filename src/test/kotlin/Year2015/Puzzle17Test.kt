7592a94712b77d1e7d7583663e17ba06e735cdbf
Angus Cheng <baller.industries@hotmail.com>
2019-02-10 20:56:56 +0800

    WIP

--- src/test/kotlin/Year2015/Puzzle17Test.kt
+++ src/test/kotlin/Year2015/Puzzle17Test.kt
@@ -25,7 +25,49 @@ class Puzzle17Test {
 
 class Puzzle17 {
 
+
+    data class InProgress(val used: List<Int>, val remaining: List<Int>) {
+
+        fun totalSize() = used.sum()
+
+        fun addContainerSize(containerSize: Int): InProgress {
+            if (!remaining.contains(containerSize)) throw RuntimeException("aaaghh noo!!!")
+
+            val newUsed = used + containerSize
+            val indexOfItemToRemove= remaining.indexOf(containerSize)
+            val newRemaining = remaining.toMutableList()
+            newRemaining.removeAt(indexOfItemToRemove)
+
+            return InProgress(newUsed, newRemaining)
+        }
+    }
+
     fun solveOne(puzzleText: String): Int {
+        val containerSizes = puzzleText.split("\n").map { it.toInt() }
+
+        var containerConfigurations = containerSizes.mapIndexed { index, it ->
+            val remainingList = containerSizes.toMutableList()
+            remainingList.removeAt(index)
+            InProgress(listOf(it), remainingList)
+        }
+
+        while (containerConfigurations.any { it.remaining.isNotEmpty() }) {
+            containerConfigurations = containerConfigurations.flatMap { containerConfiguration ->
+                containerConfiguration.remaining.map { remainingContainerSize ->
+                    containerConfiguration.addContainerSize(remainingContainerSize)
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+
+
         return 2222
     }
 
