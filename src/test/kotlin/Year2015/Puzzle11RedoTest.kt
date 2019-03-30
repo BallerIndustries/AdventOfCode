1499b0447c693e181e3bb043c34467aa56576cee
Angus Cheng <baller.industries@hotmail.com>
2019-03-30 12:30:24 +0800

    Finally got this! Arghh needed to use Longs, simplke

--- src/test/kotlin/Year2015/Puzzle11RedoTest.kt
+++ src/test/kotlin/Year2015/Puzzle11RedoTest.kt
@@ -12,13 +12,13 @@ class Puzzle11RedoTest {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(252594, result)
+        assertEquals("hepxxyzz", result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(3579328, result)
+        assertEquals("heqaabcc", result)
     }
 
     @Test
@@ -33,7 +33,6 @@ class Puzzle11RedoTest {
         assertEquals("ba", nextPassword)
     }
 
-
     @Test
     fun `password after ba should be bb`() {
         val nextPassword = puzzle.nextPassword("ba")
@@ -42,32 +41,39 @@ class Puzzle11RedoTest {
 
     @Test
     fun `password a should equal integer 1`() {
-        assertEquals(0, puzzle.passwordToInt("a"))
+        assertEquals(0, puzzle.passwordToLong("a"))
     }
 
     @Test
     fun `integer 1 should equal password a`() {
-        assertEquals("a", puzzle.intToPassword(0))
+        assertEquals("a", puzzle.longToPassword(0))
     }
 
     @Test
     fun `integer 2 should equal password b`() {
-        assertEquals("b", puzzle.intToPassword(1))
+        assertEquals("b", puzzle.longToPassword(1))
     }
 
     @Test
     fun `integer 25 should equal password z`() {
-        assertEquals("z", puzzle.intToPassword(25))
+        assertEquals("z", puzzle.longToPassword(25))
     }
 
     @Test
     fun `integer 26 should equal password ba`() {
-        assertEquals("ba", puzzle.intToPassword(26))
+        assertEquals("ba", puzzle.longToPassword(26))
     }
 
     @Test
     fun `ba should equal integer 26`() {
-        assertEquals(26, puzzle.passwordToInt("ba"))
+        assertEquals(26, puzzle.passwordToLong("ba"))
+    }
+
+    @Test
+    fun `can round trip the largest long`() {
+        val dog = puzzle.longToPassword(Long.MAX_VALUE)
+        val horse = puzzle.passwordToLong(dog)
+        assertEquals(horse, Long.MAX_VALUE)
     }
 
     @Test
@@ -75,8 +81,8 @@ class Puzzle11RedoTest {
         val random = Random(0)
 
         (0 until 100000).forEach {
-            val randomInteger = random.nextInt(0, Int.MAX_VALUE)
-            val roundTheWorld = puzzle.passwordToInt(puzzle.intToPassword(randomInteger))
+            val randomInteger = random.nextLong(0, Long.MAX_VALUE)
+            val roundTheWorld = puzzle.passwordToLong(puzzle.longToPassword(randomInteger))
             assertEquals(randomInteger, roundTheWorld)
         }
     }
@@ -114,26 +120,26 @@ class PuzzleRedo11 {
     }
 
     fun nextPassword(password: String): String {
-        return intToPassword(passwordToInt(password) + 1)
+        return longToPassword(passwordToLong(password) + 1)
     }
 
     val dog: Map<Char, Char> = mapOf('0' to 'a', '1' to 'b', '2' to 'c', '3' to 'd', '4' to 'e', '5' to 'f', '6' to 'g', '7' to 'h', '8' to 'i', '9' to 'j', 'a' to 'k', 'b' to 'l', 'c' to 'm', 'd' to 'n', 'e' to 'o', 'f' to 'p', 'g' to 'q', 'h' to 'r', 'i' to 's', 'j' to 't', 'k' to 'u', 'l' to 'v', 'm' to 'w', 'n' to 'x', 'o' to 'y', 'p' to 'z')
     val invertedDog: Map<Char, Char> = dog.entries.map { (key, value) -> value to key }.toMap()
 
-    fun passwordToInt(password: String): Int {
-        return password.map { invertedDog[it]!! }.joinToString("").toInt(26)
+    fun passwordToLong(password: String): Long {
+        return password.map { invertedDog[it]!! }.joinToString("").toLong(26)
     }
 
-    fun intToPassword(number: Int): String {
+    fun longToPassword(number: Long): String {
         return number.toString(26).map { dog[it]!! }.joinToString("")
     }
 
     fun solveOne(puzzleText: String): String {
-        val initialPasswordAsInteger = puzzleText.toInt(26)
+        val initialPasswordAsInteger = passwordToLong(puzzleText)
 
-        (initialPasswordAsInteger until Int.MAX_VALUE).forEach { number ->
+        (initialPasswordAsInteger until Long.MAX_VALUE).forEach { number ->
 
-            val password = intToPassword(number)
+            val password = longToPassword(number)
 
             if (hasNoIOOrL(password) && hasTwoNonOverlappingPairs(password) && hasThreeCharStraight(password)) {
                 return password
@@ -143,9 +149,8 @@ class PuzzleRedo11 {
         throw RuntimeException("I can't even!")
     }
 
-    fun solveTwo(puzzleText: String): Int {
-
-
-        return 1281928
+    fun solveTwo(puzzleText: String): String {
+        val firstPassword = solveOne(puzzleText)
+        return solveOne(nextPassword(firstPassword))
     }
 }
