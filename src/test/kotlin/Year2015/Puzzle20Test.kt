80a91bb456cee8ac3d80249e6628f25bbc990850
Angus Cheng <baller.industries@hotmail.com>
2019-04-21 00:53:13 +0800

    got it yo

--- src/test/kotlin/Year2015/Puzzle20Test.kt
+++ src/test/kotlin/Year2015/Puzzle20Test.kt
@@ -2,37 +2,34 @@ package Year2015
 
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import java.lang.RuntimeException
-import java.util.*
-import kotlin.random.Random
 
 class Puzzle20Test {
     val puzzle = Puzzle20()
     val puzzleText = this::class.java.getResource("/2015/puzzle20.txt").readText().replace("\r", "")
-    val exampleText = """
-    """.trimIndent()
+    val exampleText = """150"""
 
     @Test
     fun `example part a`() {
         val result = puzzle.solveOne(exampleText)
-        assertEquals(4, result)
+        assertEquals(8, result)
     }
 
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(509, result)
+        assertEquals(831600, result)
     }
 
     @Test
     fun `puzzle part b`() {
+        //3603600 too high
         val result = puzzle.solveTwo(puzzleText)
         assertEquals(205, result)
     }
-
 }
 
 class Puzzle20 {
+    // TODO: Memoize
     fun getMultiples(number: Int): List<Int> {
         return (1..number).filter { aCandidateForTheOfficeOfMultiple ->
             number % aCandidateForTheOfficeOfMultiple == 0
@@ -42,18 +39,14 @@ class Puzzle20 {
     fun solveOne(puzzleText: String): Int {
         val magicNumber = puzzleText.toInt()
 
-//        val numberToMultiples = generateMemo(magicNumber)
-
-
-        (500_000 until Int.MAX_VALUE).forEach { houseNumber ->
-
+        (800000 until Int.MAX_VALUE).forEach { houseNumber ->
             val numPresentsReceived = getMultiples(houseNumber).map { it * 10 }.sum()
 
             if (houseNumber % 1000 == 0) {
                 println("houseNumber = $houseNumber numPresentsReceived = $numPresentsReceived")
             }
 
-            if (numPresentsReceived > magicNumber) {
+            if (numPresentsReceived >= magicNumber) {
                 return houseNumber
             }
         }
@@ -61,29 +54,29 @@ class Puzzle20 {
         throw RuntimeException("Summin strange be happnin")
     }
 
-    private fun generateMemo(magicNumber: Int): Map<Int, MutableSet<Int>> {
-        val jur = mutableMapOf<Int, MutableSet<Int>>()
+    //TODO: Do this better you butthead
+    fun solveTwo(puzzleText: String): Int {
+        val magicNumber = puzzleText.toInt()
+        val housesToGifts = mutableMapOf<Int, Int>()
+        var minHouse = Int.MAX_VALUE
 
-        (1 .. magicNumber).forEach { multiple ->
+        // TODO: Should move up by the house number
+        (1 until Int.MAX_VALUE).forEach { elfNumber ->
 
-            if (multiple % 10 == 0) {
-                println(multiple)
-            }
+            val firstFiftyHouses = (1 .. 50).map { it * elfNumber }
+            val prezziesToDeliver = 11 * elfNumber
 
-            var currentNumber = multiple
+            firstFiftyHouses.forEach { houseNumber ->
+                val giftsAtThisHouse = (housesToGifts[houseNumber] ?: 0) + prezziesToDeliver
+                housesToGifts[houseNumber] = giftsAtThisHouse
 
-            while (currentNumber < magicNumber) {
-                val setOfMultiples = jur[currentNumber] ?: mutableSetOf()
-                setOfMultiples.add(currentNumber)
-                jur[currentNumber] = setOfMultiples
-                currentNumber += multiple
+                if (giftsAtThisHouse > magicNumber && houseNumber < minHouse) {
+                    minHouse = houseNumber
+                    println(houseNumber)
+                }
             }
         }
 
-        return jur
-    }
-
-    fun solveTwo(puzzleText: String): Int {
-        return 222
+        throw RuntimeException("asoidjas")
     }
 }
