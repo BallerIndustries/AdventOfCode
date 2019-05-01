8c513ea2498a97679b63a4a57281b5cd2f1e4f46
anguruso <baller.industries@hotmail.com>
2019-05-01 12:50:22 +0800

    DFS please

--- src/test/kotlin/Year2015/Puzzle24Test.kt
+++ src/test/kotlin/Year2015/Puzzle24Test.kt
@@ -1,5 +1,6 @@
 package Year2015
 
+import Year2015.Puzzle24.InProgress.Companion.startingPoint
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 import java.lang.RuntimeException
@@ -24,11 +25,45 @@ class Puzzle24Test {
 
 class Puzzle24 {
 
+    data class InProgress(val used: Set<Int>, val remaining: Set<Int>) {
+
+        companion object {
+            fun startingPoint(number: Int, allNumbers: List<Int>): InProgress {
+              return InProgress(setOf(number), (allNumbers - number).toSet())
+            }
+        }
+
+        fun tickAlong(targetSum: Int): List<InProgress> {
+            return remaining.mapNotNull { remainingNumber ->
+                val newUsed = this.used + remainingNumber
+                val newRemaining = this.remaining - remainingNumber
+                val sum = newUsed.sum()
+
+                if (sum == targetSum) {
+                    null
+                }
+                else if (sum > targetSum) {
+                    null
+                }
+                else {
+                    InProgress(newUsed, newRemaining)
+                }
+            }
+        }
+    }
+
+
     fun solveOne(puzzleText: String): Int {
-        val weights = puzzleText.split("\n").map { it.toInt() }
+        val weights = puzzleText.split("\n").map { it.toInt() }.sortedDescending()
         val compartmentWeight = weights.sum() / 3
 
+        var inProgressStuff = weights.map { startingPoint(it, weights) }
 
+//        while (inProgressStuff.any { it.remaining.isNotEmpty() }) {
+//            inProgressStuff = inProgressStuff.flatMap { inProgress -> inProgress.tickAlong(compartmentWeight) }
+//        }
+
+        // Lets do a depth first search instead
 
         return 222
     }
