bd829cf8fe275e771a5470ea658e4c590f189793
anguruso <baller.industries@hotmail.com>
2019-05-09 00:00:19 +0800

    Originally did this with a dictionary, was too slow. Now using bool[][], fast. Nice.

---
+++ src/test/kotlin/Year2016/Puzzle18Test.kt
@@ -0,0 +1,78 @@
+package Year2016
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
+import java.lang.RuntimeException
+
+class Puzzle18Test {
+    val puzzle = Puzzle18()
+    val puzzleText = this::class.java.getResource("/2016/puzzle18.txt").readText().replace("\r", "")
+
+    @Test
+    fun `can solve part a`() {
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(1951, result)
+    }
+
+    @Test
+    fun `can solve part b`() {
+        val result= puzzle.solveTwo(puzzleText)
+        assertEquals(20002936, result)
+    }
+}
+
+class Puzzle18 {
+    data class Point(val x: Int, val y: Int) {
+        fun up() = this.copy(y = this.y - 1)
+        fun left() = this.copy(x = this.x - 1)
+        fun right() = this.copy(x = this.x + 1)
+    }
+
+    fun isTrap(buffer: Array<Array<Boolean>>, point: Point, maxX: Int): Boolean {
+        if (point.x < 0 || point.x >= maxX) {
+            return false
+        }
+
+        return buffer[point.y][point.x]
+    }
+
+    fun solveOne(puzzleText: String, rowCount: Int = 40): Int {
+        val buffer = Array(rowCount) { Array( puzzleText.length) {false} }
+
+        puzzleText.forEachIndexed { index, char ->
+            buffer[0][index] = char == '^'
+        }
+
+        (1 until rowCount).forEach { y ->
+            (0 until puzzleText.length).forEach { x ->
+
+                val thisPoint = Point(x, y)
+                val upPoint = thisPoint.up()
+
+                val left = isTrap(buffer, upPoint.left(), puzzleText.length)
+                val center = isTrap(buffer, upPoint, puzzleText.length)
+                val right = isTrap(buffer, upPoint.right(), puzzleText.length)
+
+                val thisJerkIsATrap = when {
+                    left && center && !right -> true
+                    right && center && !left -> true
+                    left && !right && !center -> true
+                    right && !center && !left -> true
+                    else -> false
+                }
+
+                buffer[y][x] = thisJerkIsATrap
+            }
+
+            if (y % 10000 == 0) println(y)
+        }
+
+        return buffer.sumBy { it.count { !it  } }
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        return solveOne(puzzleText, 400000)
+    }
+}
