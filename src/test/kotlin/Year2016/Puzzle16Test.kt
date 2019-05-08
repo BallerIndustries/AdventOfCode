3d5c0b88e145abae7541f3d243bde3301f7927c1
anguruso <baller.industries@hotmail.com>
2019-05-08 20:53:59 +0800

    Using a stingbuilder, and generating data in a more efficient way. Fantastic victory

--- src/test/kotlin/Year2016/Puzzle16Test.kt
+++ src/test/kotlin/Year2016/Puzzle16Test.kt
@@ -16,6 +16,7 @@ class Puzzle16Test {
     @Test
     fun `can solve part b`() {
         val checksum: String = puzzle.solveTwo(puzzleText)
+        assertEquals("01101011101100011", checksum)
     }
 
     @Test
@@ -26,9 +27,9 @@ class Puzzle16Test {
     }
 
     @Test
-    fun `generated data that fills a disk of length 20 with an initial state of 10000`() {
-        val actual = puzzle.generateData("10000", 20)
-        val expected = "10000011110010000111110"
+    fun `next gen generated data that fills a disk of length 20 with an initial state of 10000`() {
+        val actual = puzzle.generateDataNextGen("10000", 20)
+        val expected = "10000011110010000111"
         assertEquals(expected, actual)
     }
 }
@@ -36,43 +37,55 @@ class Puzzle16Test {
 class Puzzle16 {
     fun solveOne(puzzleText: String, length: Int = 272): String {
         var generatedData = puzzleText
-        generatedData = generateData(generatedData, length)
+        generatedData = generateDataNextGen(generatedData, length)
 
-        return calculateChecksum(generatedData.substring(0, length))
+        return calculateChecksum(generatedData)
     }
 
-    fun generateData(generatedData: String, length: Int): String {
-        var generatedData1 = generatedData
+    fun generateDataNextGen(intitialData: String, length: Int): String {
         val dog = mapOf('0' to '1', '1' to '0')
+        val buffer = StringBuilder(length)
+        buffer.append(intitialData)
 
-        while (generatedData1.length < length) {
-            generatedData1 = generatedData1 + "0" + generatedData1.reversed().map { dog[it]!! }.joinToString("")
+        while (buffer.length < length) {
+            val newBuffer = StringBuilder(buffer.count())
+
+            var index = buffer.lastIndex
+
+            while (index >= 0) {
+                newBuffer.append(dog[buffer.get(index)])
+                index--
+            }
+
+            buffer.append('0')
+            buffer.append(newBuffer)
         }
-        return generatedData1
+
+        return buffer.toString().substring(0, length)
     }
 
     fun calculateChecksum(randomData: String): String {
         var crazyData = randomData
-        var buffer = ""
+        val buffer = StringBuilder(randomData.length)
 
         while (true) {
             for (index in 0 .. crazyData.lastIndex step 2) {
                 val l = crazyData[index]
                 val r = crazyData[index + 1]
 
-                if (l == r) buffer += "1" else buffer += "0"
+                if (l == r) buffer.append('1') else buffer.append('0')
             }
 
             if (buffer.length % 2 == 1) {
-                return buffer
+                return buffer.toString()
             }
 
-            crazyData = buffer
-            buffer = ""
+            crazyData = buffer.toString()
+            buffer.clear()
         }
     }
 
     fun solveTwo(puzzleText: String): String {
-        return solveOne(puzzleText, 35651584)
+        return solveOne(puzzleText, 35_651_584)
     }
 }
