4d105d98ba95bb277e38c73b9a40fa11947a2312
anguruso <baller.industries@hotmail.com>
2019-05-12 16:36:04 +0800

    WIP

---
+++ src/test/kotlin/Year2016/Puzzle21Test.kt
@@ -0,0 +1,79 @@
+package Year2016
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.lang.RuntimeException
+
+class Puzzle21Test {
+    val puzzle = Puzzle21()
+    val puzzleText = this::class.java.getResource("/2016/puzzle21.txt").readText().replace("\r", "")
+
+    @Test
+    fun `can solve part a`() {
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(22887907, result)
+    }
+
+    @Test
+    fun `can solve part b`() {
+        // 108 is too low
+        val result= puzzle.solveTwo(puzzleText)
+        assertEquals(109, result)
+    }
+}
+
+class Puzzle21 {
+    class SwapPosition(val from: Int, val to: Int) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class SwapLetter(val from: Char, val to: Char) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class RotateLeft(val steps: Int) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class RotateRight(val steps: Int) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class RotateBasedOnPosition(val letter: Char) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class ReversePositions(val from: Int, val to: Int) : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class MovePosition() : Operation {
+        override fun execute(text: String): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    interface Operation {
+        fun execute(text: String): String
+    }
+
+    fun solveOne(puzzleText: String): Long {
+        return 200L
+    }
+
+    fun solveTwo(puzzleText: String): Long {
+        return 100L
+    }
+}
