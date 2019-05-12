e95817e7126fe19a4959fd38011059f0dd062b14
Angus Cheng <baller.industries@hotmail.com>
2019-05-13 02:43:01 +0800

    Woo puzzle22 done. Pretty print for the win

--- src/test/kotlin/Year2016/Puzzle22Test.kt
+++ src/test/kotlin/Year2016/Puzzle22Test.kt
@@ -12,19 +12,27 @@ class Puzzle22Test {
         // 1995 too high
         // 1007 too high
         val result = puzzle.solveOne(puzzleText)
-        assertEquals("gbhcefad", result)
+        assertEquals(981, result)
     }
 
     @Test
     fun `can solve part b`() {
-        // 108 is too low
-        val result= puzzle.solveTwo(puzzleText)
-        assertEquals("gahedfcb", result)
+        // 233 too high
+        puzzle.solveTwo(puzzleText)
+        //// Get in front of the far right node
+        //
+        //35 // to top
+        //67 // to in front of node
+        //
+        //// Move to the far left
+        //// Grid is 35 across
+        //
+        //(33 * 5) + 1 + 67
     }
 }
 
 class Puzzle22 {
-    data class Node(val name: String, val size: Int, val used: Int, val available: Int) {
+    data class Node(val name: String, val size: Int, val used: Int, val available: Int, val position: Point, val hasTheGoldenData: Boolean) {
         fun isNotEmpty() = this.used != 0
     }
 
@@ -33,7 +41,12 @@ class Puzzle22 {
     fun solveOne(puzzleText: String): Int {
         val grid = parseGrid(puzzleText)
         val nodes = grid.values.toList()
-        var count = 0
+        val viablePairs = getViablePairs(nodes)
+        return viablePairs.size
+    }
+
+    private fun getViablePairs(nodes: List<Node>): MutableList<Pair<Node, Node>> {
+        val viablePairs = mutableListOf<Pair<Node, Node>>()
 
         for (index in 0 until nodes.size) {
             for (jindex in 0 until nodes.size) {
@@ -43,12 +56,12 @@ class Puzzle22 {
                 val nodeB = nodes[jindex]
 
                 if (nodeA.isNotEmpty() && nodeB.available > nodeA.used) {
-                    count++
+                    viablePairs.add(nodeA to nodeB)
                 }
             }
         }
 
-        return count
+        return viablePairs
     }
 
     private fun parseGrid(puzzleText: String): Map<Point, Node> {
@@ -56,23 +69,52 @@ class Puzzle22 {
                 .let { lines -> lines.subList(2, lines.size) }
                 .map { line ->
                     val tmp = line.split(Regex("\\s"))
-                    val cheese = tmp.mapNotNull { it.replace("T", "").toIntOrNull() }
-                    Node(tmp[0], cheese[0], cheese[1], cheese[2])
+                    val nodeName = tmp[0]
+                    val theNumbers = tmp.mapNotNull { it.replace("T", "").toIntOrNull() }
+                    val (x, y) = nodeName.replace("/dev/grid/node-", "")
+                            .replace("x", "")
+                            .replace("y", "")
+                            .split("-")
+                            .map { it.toInt() }
+
+                    Node(nodeName, theNumbers[0], theNumbers[1], theNumbers[2], Point(x, y), false)
                 }
 
-        val grid = nodes.associate { node ->
-            val (x, y) = node.name.replace("/dev/grid/node-", "")
-                    .replace("x", "")
-                    .replace("y", "")
-                    .split("-")
-                    .map { it.toInt() }
+        val jur = nodes.associate { node -> node.position to node }.toMutableMap()
+        val maxX = jur.keys.maxBy { it.x }!!.x
+        val goldenIndex = Point(maxX, 0)
+        jur[goldenIndex] = jur[goldenIndex]!!.copy(hasTheGoldenData = true)
 
-            Point(x, y) to node
+        return jur
+    }
+
+    fun paddyPaddyPadPad(text: String): String {
+        return when {
+            text.length == 1 -> "  $text"
+            text.length == 2 -> " $text"
+            else -> text
         }
-        return grid
     }
 
     fun solveTwo(puzzleText: String): String {
+        val grid = parseGrid(puzzleText)
+
+        val maxX = grid.keys.maxBy { it.x }!!.x
+        val maxY = grid.keys.maxBy { it.y }!!.y
+
+        val dog = (0 .. maxY).map { y ->
+            (0 .. maxX).map { x ->
+
+                val node = grid[Point(x, y)]!!
+                val partA = paddyPaddyPadPad(node.used.toString())
+                val partB = paddyPaddyPadPad((node.used + node.available).toString())
+                val cheese = "[$partA / $partB]"
+                cheese
+            }.joinToString(" ")
+        }.joinToString("\n")
+
+        println(dog)
+
         return "22323"
     }
 }
