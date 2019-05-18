d7e8ca928e9cab2c3215cb497a8e19c79aa0231a
anguruso <baller.industries@hotmail.com>
2019-05-19 05:06:11 +0800

    nice

---
+++ src/test/kotlin/Year2016/Puzzle25Test.kt
@@ -0,0 +1,38 @@
+package Year2016
+
+import org.junit.Assert.assertEquals
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.random.Random
+
+class Puzzle25Test {
+    val puzzle = Puzzle25()
+    val puzzleText = this::class.java.getResource("/2016/puzzle25.txt").readText().replace("\r", "")
+
+
+    @Test
+    fun `a aa a a aaa`() {
+
+
+        fun horseHorse(target: Int): Int {
+
+            var n = 1
+            while (n < target) {
+                if (n % 2 == 0) {
+                    n = n * 2 + 1
+                }
+                else {
+                    n *= 2
+                }
+            }
+
+            return n - target
+        }
+
+        val target = 9 * 282
+        println(horseHorse(target))
+    }
+}
+
+class Puzzle25 {
+}
