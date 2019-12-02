5feb2864e0449ad4757888c9844bc00c1ae56672
baller.industries@hotmail.com <mashgood>
2019-12-02 20:33:41 +0800

    Did this at lunch time at work, but did it on my work puter so wasn't able to commit it. That's right I coded it again

--- src/test/kotlin/Year2019/Puzzle2Test.kt
+++ src/test/kotlin/Year2019/Puzzle2Test.kt
@@ -2,6 +2,7 @@ package Year2019
 
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import java.lang.RuntimeException
 
 class Puzzle2Test {
     val puzzleText = Puzzle2Test::class.java.getResource("/2019/puzzle2.txt").readText().replace("\r", "")
@@ -10,23 +11,96 @@ class Puzzle2Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(454, result)
+        assertEquals(3267740, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(566, result)
+        assertEquals(7870, result)
     }
 }
 
 class Puzzle2 {
-    fun solveOne(puzzleText: String): String {
-        throw NotImplementedError()
+    data class State(val list: List<Long>, val programCounter: Int = 0, val isHalted: Boolean = false) {
+        fun writeToIndex(index: Int, value: Long): State {
+            val newList = this.list.mapIndexed { i, it -> if (i == index) value else it }
+            return this.copy(list = newList)
+        }
+
+        fun halt(): State {
+            return this.copy(isHalted =  true)
+        }
+
+        fun incrementProgramCounter(amount: Int): State {
+            return this.copy(programCounter = this.programCounter + amount)
+        }
     }
 
-    fun solveTwo(puzzleText: String): String {
-        throw NotImplementedError()
+    interface Instruction {
+        fun execute(state: State): State
+    }
+
+    data class AddInstruction(val indexA: Int, val indexB: Int, val destinationIndex: Int): Instruction {
+        override fun execute(state: State): State {
+            val result = state.list[indexA] + state.list[indexB]
+            return state.writeToIndex(destinationIndex, result)
+        }
+    }
+
+    data class MultiplyInstruction(val indexA: Int, val indexB: Int, val destinationIndex: Int): Instruction {
+        override fun execute(state: State): State {
+            val result = state.list[indexA] * state.list[indexB]
+            return state.writeToIndex(destinationIndex, result)
+        }
+    }
+
+    class HaltInstruction(): Instruction {
+        override fun execute(state: State): State {
+            return state.halt()
+        }
+    }
+
+    fun solveOne(puzzleText: String, noun: Long = 12, verb: Long = 2): Long {
+        val list = puzzleText.split(",").map { it.toLong() }
+        var state = State(list, 0, false).writeToIndex(1, noun).writeToIndex(2, verb)
+
+        while (!state.isHalted) {
+            val instruction = when(state.list[state.programCounter]) {
+                1L -> {
+                    val indexA = state.list[state.programCounter + 1].toInt()
+                    val indexB = state.list[state.programCounter + 2].toInt()
+                    val destinationIndex = state.list[state.programCounter + 3].toInt()
+                    AddInstruction(indexA, indexB, destinationIndex)
+                }
+                2L -> {
+                    val indexA = state.list[state.programCounter + 1].toInt()
+                    val indexB = state.list[state.programCounter + 2].toInt()
+                    val destinationIndex = state.list[state.programCounter + 3].toInt()
+                    MultiplyInstruction(indexA, indexB, destinationIndex)
+                }
+                99L -> HaltInstruction()
+                else -> throw RuntimeException("osidhfosidhjf")
+            }
+
+            state = instruction.execute(state).incrementProgramCounter(4)
+        }
+
+        return state.list.first()
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        (0..99).forEach { noun ->
+            (0..99).forEach { verb ->
+                val result = solveOne(puzzleText, noun.toLong(), verb.toLong())
+
+                if (result == 19690720L) {
+                    return 100 * noun + verb
+                }
+            }
+        }
+
+        throw RuntimeException("sdofijsd")
     }
 }
 
