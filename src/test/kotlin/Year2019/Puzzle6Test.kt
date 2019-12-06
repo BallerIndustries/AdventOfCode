75792358442cc2e9ac4b3c892dc8a48233af9c56
baller.industries@hotmail.com <mashgood>
2019-12-06 23:07:13 +0800

    Did this on my work comp, doing it again

--- src/test/kotlin/Year2019/Puzzle6Test.kt
+++ src/test/kotlin/Year2019/Puzzle6Test.kt
@@ -10,23 +10,90 @@ class Puzzle6Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals("a", result)
+        assertEquals(270768, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals("a", result)
+        assertEquals(451, result)
     }
 }
 
 class Puzzle6 {
-    fun solveOne(puzzleText: String): String {
-        throw NotImplementedError()
+    data class Node(val name: String, val parentName: String?, val children: List<String>) {
+        fun addChild(name: String): Node {
+            return this.copy(children = this.children + name)
+        }
+
+        fun countStepsTo(graph: MutableMap<String, Node>, s: String): Int {
+            var currentNode = this
+            var steps = 0
+
+            while (currentNode.name != s) {
+                currentNode = graph[currentNode.parentName]!!
+                steps++
+            }
+
+            return steps
+        }
+
+        fun getAllNodesUntil(graph: Map<String, Node>, s: String): List<Node> {
+            var currentNode = this
+            val list = mutableListOf<Node>()
+
+            while (currentNode.name != s) {
+                currentNode = graph[currentNode.parentName]!!
+                list.add(currentNode)
+            }
+
+            return list
+        }
     }
 
-    fun solveTwo(puzzleText: String): String {
-        throw NotImplementedError()
+    fun parseGraph(puzzleText: String): MutableMap<String, Node> {
+        val nameToNode = mutableMapOf<String, Node>()
+
+        // Pares all child nodes
+        puzzleText.split("\n").forEach { line ->
+            val (parentName, nodeName) = line.split(")")
+
+            if (!nameToNode.containsKey(nodeName)) {
+                nameToNode[nodeName] = Node(nodeName, parentName, listOf())
+            }
+            else {
+                throw RuntimeException("sodifjsdf")
+            }
+        }
+
+        // Link up parent nodes to children
+        puzzleText.split("\n").forEach { line ->
+
+            val (parentName, nodeName) = line.split(")")
+            nameToNode[parentName] = (nameToNode[parentName] ?: Node(parentName, null, listOf())).addChild(nodeName)
+        }
+
+
+        return nameToNode
+    }
+
+
+    fun solveOne(puzzleText: String): Int {
+        val graph = parseGraph(puzzleText)
+        return graph.values.sumBy { node: Node -> node.countStepsTo(graph, "COM") }
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        val graph = parseGraph(puzzleText)
+        val santaNode = graph.values.find { it.name == "SAN" }!!
+        val myNode = graph.values.find { it.name == "YOU" }!!
+
+        val santaToComPath: List<Node> = santaNode.getAllNodesUntil(graph, "COM")
+        val myToComPath: List<Node> = myNode.getAllNodesUntil(graph, "COM")
+
+        val lowestCommonParent = santaToComPath.first { santaNode -> myToComPath.contains(santaNode) }
+
+        return santaNode.countStepsTo(graph, lowestCommonParent.name) + myNode.countStepsTo(graph, lowestCommonParent.name) - 2
     }
 }
 
