538b27ee1c1f74d6eb4b4b07755b9647ae1e9b73
baller.industries@hotmail.com <mashgood>
2019-12-10 19:58:56 +0800

    Doing this again because I originally did it inside a swiss bank

--- src/test/kotlin/Year2019/Puzzle10Test.kt
+++ src/test/kotlin/Year2019/Puzzle10Test.kt
@@ -10,23 +10,121 @@ class Puzzle10Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals("a", result)
+        assertEquals(309, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals("a", result)
+        assertEquals(416, result)
+    }
+
+    @Test
+    fun `part b example a`() {
+        val text = """
+            .#..##.###...#######
+            ##.############..##.
+            .#.######.########.#
+            .###.#######.####.#.
+            #####.##.#.##.###.##
+            ..#####..#.#########
+            ####################
+            #.####....###.#.#.##
+            ##.#################
+            #####.##.###..####..
+            ..######..##.#######
+            ####.##.####...##..#
+            .#####..#.######.###
+            ##...#.##########...
+            #.##########.#######
+            .####.#.###.###.#.##
+            ....##.##.###..#####
+            .#.#.###########.###
+            #.#.#.#####.####.###
+            ###.##.####.##.#..##
+        """.trimIndent()
+
+        val result = puzzle.solveTwo(text)
+        assertEquals(802, result)
     }
 }
 
 class Puzzle10 {
-    fun solveOne(puzzleText: String): String {
-        throw NotImplementedError()
+
+    data class Point(val x: Int, val y: Int) {
+        fun rotationTo(other: Point): Double {
+            return Math.atan2(this.y.toDouble() - other.y.toDouble(), this.x.toDouble() - other.x.toDouble()) * (180.0 / Math.PI)
+        }
+
+        fun manhattanDistanceTo(other: Point): Int {
+           return Math.abs(this.x - other.x) + Math.abs(this.y - other.y)
+        }
     }
 
-    fun solveTwo(puzzleText: String): String {
-        throw NotImplementedError()
+    fun solveOne(puzzleText: String): Int? {
+        val asteroids = getAsteroids(puzzleText)
+        val distinctRotationsToAsteroid = getRotationCountPerAsteroid(asteroids)
+        return distinctRotationsToAsteroid.map { it.first }.max()
+    }
+
+    private fun getRotationCountPerAsteroid(asteroids: List<Point>): List<Pair<Int, Point>> {
+        val distinctRotationsToAsteroid = asteroids.map { thisAsteroid ->
+            val allOtherAsteroids = asteroids.filter { it != thisAsteroid }
+
+            // Get angle of rotation for this asteroid to all the others
+            val rotations = allOtherAsteroids.map { thisAsteroid.rotationTo(it) }.distinct()
+
+            rotations.size to thisAsteroid
+        }
+        return distinctRotationsToAsteroid
+    }
+
+    private fun getAsteroids(puzzleText: String): List<Point> {
+        val asteroids = puzzleText.split("\n").mapIndexed { y, line ->
+            line.mapIndexed { x, char ->
+                Point(x, y) to char
+            }
+        }.flatten().filter {
+            it.second == '#'
+        }.map { it.first }
+        return asteroids
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        val asteroids = getAsteroids(puzzleText)
+        val distinctRotationsToAsteroid = getRotationCountPerAsteroid(asteroids)
+
+        val positionOfLaser = distinctRotationsToAsteroid.maxBy { it.first }!!.second
+
+        val pointRotationAndDistance = asteroids.filter {
+            it != positionOfLaser
+        }.map {
+            Triple(it, positionOfLaser.rotationTo(it), positionOfLaser.manhattanDistanceTo(it))
+        }.sortedWith(compareBy({ it.second }, { it.third })).toMutableList()
+
+        var zappedAsteroids = 0
+        var lastZappedAsteroid: Triple<Point, Double, Int>? = null
+        var index = pointRotationAndDistance.indexOfFirst { it.second >= 90.0 }
+
+        do {
+            if (index == -1) {
+                index = 0
+            }
+
+            lastZappedAsteroid = pointRotationAndDistance[index]
+            pointRotationAndDistance.removeAt(index)
+            zappedAsteroids++
+
+            index = pointRotationAndDistance.indexOfFirst { it.second > lastZappedAsteroid.second }
+
+            if (index == -1) {
+                index = 0
+            }
+
+            println("$zappedAsteroids vaporised at ${lastZappedAsteroid.first}")
+        } while (zappedAsteroids < 200)
+
+        return lastZappedAsteroid!!.first.x * 100 + lastZappedAsteroid.first.y
     }
 }
 
