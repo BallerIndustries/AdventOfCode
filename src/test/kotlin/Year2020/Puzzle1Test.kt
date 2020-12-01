cbf07df541d56c063bbd4a7456b9ec400934b11e
Angus Cheng <baller.industries@hotmail.com>
2020-12-01 13:15:26 +0800

    day one gg

--- src/test/kotlin/Year2020/Puzzle1Test.kt
+++ src/test/kotlin/Year2020/Puzzle1Test.kt
@@ -8,25 +8,88 @@ class Puzzle1Test {
     val puzzle = Puzzle1()
 
     @Test
+    fun `example part a`() {
+        val puzzleText = "1721\n" +
+                "979\n" +
+                "366\n" +
+                "299\n" +
+                "675\n" +
+                "1456"
+
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(514579, result)
+    }
+
+    @Test
+    fun `example part b`() {
+        val puzzleText = "1721\n" +
+                "979\n" +
+                "366\n" +
+                "299\n" +
+                "675\n" +
+                "1456"
+
+        val result = puzzle.solveTwo(puzzleText)
+        assertEquals(241861950, result)
+    }
+
+    @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(3404722, result)
+        assertEquals(964875, result)
     }
 
     @Test
     fun `puzzle part b`() {
         val result = puzzle.solveTwo(puzzleText)
-        assertEquals(5104215, result)
+        assertEquals(158661360, result)
     }
 }
 
 class Puzzle1 {
     fun solveOne(puzzleText: String): Int {
-        return 1337
+        val ints: List<Int> = puzzleText.split("\n").map { it.toInt() }
+        var i = 0
+        var j = 1
+
+        while (i < ints.size) {
+            while (j < ints.size) {
+                if (i != j && ints[i] + ints[j] == 2020) {
+                    return ints[i] * ints[j]
+                }
+                j++
+            }
+
+            j=0;
+            i++;
+        }
+
+        throw RuntimeException()
     }
 
     fun solveTwo(puzzleText: String): Int {
-        return 420
+        val ints: List<Int> = puzzleText.split("\n").map { it.toInt() }
+        var i = 0
+        var j = 1
+        var k = 2;
+
+        while (i < ints.size) {
+            j = 0
+            while (j < ints.size) {
+                k = 0
+                while (k < ints.size) {
+                    if (i != j && i != k && ints[i] + ints[j] + ints[k] == 2020) {
+                        return ints[i] * ints[j] * ints[k]
+                    }
+                    k++
+                }
+                j++
+            }
+
+            i++;
+        }
+
+        throw RuntimeException()
     }
 }
 
