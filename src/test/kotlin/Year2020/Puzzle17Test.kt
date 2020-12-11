2f25a1d060babff2107e650678f77981bfaab620
Angus Cheng <baller.industries@hotmail.com>
2020-12-11 12:54:35 +0800

    more temlpates

---
+++ src/test/kotlin/Year2020/Puzzle17Test.kt
@@ -0,0 +1,46 @@
+package Year2020
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Puzzle17Test {
+    val puzzleText = this::class.java.getResource("/2020/puzzle17.txt").readText().replace("\r", "")
+    val puzzle = Puzzle17()
+
+    @Test
+    fun `puzzle part a`() {
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(964875, result)
+    }
+
+    @Test
+    fun `puzzle part b`() {
+        val result = puzzle.solveTwo(puzzleText)
+        assertEquals(158661360, result)
+    }
+
+    @Test
+    fun `example part a`() {
+        val puzzleText = ""
+        val result = puzzle.solveOne(puzzleText)
+        assertEquals(514579, result)
+    }
+
+    @Test
+    fun `example part b`() {
+        val puzzleText = ""
+        val result = puzzle.solveTwo(puzzleText)
+        assertEquals(241861950, result)
+    }
+}
+
+class Puzzle17 {
+    fun solveOne(puzzleText: String): Int {
+        return 1
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        return 1
+    }
+}
+
