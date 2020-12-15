7e12d6a33d5161d5b012795bda3bbaabdf1d4af6
Angus Cheng <baller.industries@hotmail.com>
2020-12-15 15:57:40 +0800

    gg

--- src/test/kotlin/Year2020/Puzzle15Test.kt
+++ src/test/kotlin/Year2020/Puzzle15Test.kt
@@ -39,124 +39,57 @@ class Puzzle15Test {
         (0 until 30_000_000).forEach {
             number++
         }
-
-
-
-
     }
 }
 
 class Puzzle15 {
-    data class LastTwo(val indexes: List<Int>) {
+    class LastTwo(var index0: Int, var index1: Int?) {
         fun add(index: Int): LastTwo {
-            val newList = (listOf(index) + this.indexes).subList(0, 2)
-            return this.copy(indexes = newList)
-        }
-    }
-
-    data class LastTwoV2(val index0: Int?, val index1: Int?) {
-        fun add(index: Int): LastTwoV2 {
-            return if (index0 == null) {
-                this.copy(index0 = index)
-            } else
-                this.copy(index0 = index, index1 = this.index0)
-        }
-
-        fun size(): Int {
-            return listOfNotNull(index0, index1).count()
-        }
-    }
-
-    class LastTwoV3(var index0: Int?, var index1: Int?) {
-        fun add(index: Int): LastTwoV3 {
             this.index1 = this.index0
             this.index0 = index
             return this
         }
-//            return if (index0 == null) {
-//                this.copy(index0 = index)
-//            } else
-//                this.copy(index0 = index, index1 = this.index0)
-//        }
 
         fun size(): Int {
-            return listOfNotNull(index0, index1).count()
+            return when (index1) {
+                null -> 1
+                else -> 2
+            }
         }
     }
 
-    fun solveOne(puzzleText: String): Int {
+    private fun solve(endTurn: Int, puzzleText: String): Int {
         val numbers = puzzleText.split(",").map { it.toInt() }
         val numbersToLastTwo = numbers.mapIndexed { index, number ->
-            number to LastTwoV2(index+1, null)
+            number to LastTwo(index+1, null)
         }.toMap().toMutableMap()
 
         var turn = numbers.size + 1
-        var lastNumber = numbers.last()
+        var currentNumber = numbers.last()
 
+        while (turn <= endTurn) {
+            val lastTwo = numbersToLastTwo[currentNumber]!!
 
-        while (true) {
-            val lastTwo = numbersToLastTwo[lastNumber]!!
-
-            val currentNumber = if (lastTwo.size() < 2) {
-                0
-            } else if (lastTwo.size() == 2) {
-                lastTwo.index0!! - lastTwo.index1!!
-            }
-            else {
-                throw RuntimeException()
-            }
-
-            println("Turn = $turn currentNumber = $currentNumber")
-            val jurness = numbersToLastTwo[currentNumber]
-            numbersToLastTwo[currentNumber] = jurness?.add(turn) ?: LastTwoV2(index0 = turn, index1 = null)
-
-            if (turn == 2020) {
-                return currentNumber
-            }
-
-            turn++
-            lastNumber = currentNumber
-        }
-    }
-
-    fun solveTwo(puzzleText: String): Int {
-        val numbers = puzzleText.split(",").map { it.toInt() }
-        val numbersToLastTwo = numbers.mapIndexed { index, number ->
-            number to LastTwoV3(index+1, null)
-        }.toMap().toMutableMap()
-
-        var turn = numbers.size + 1
-        var lastNumber = numbers.last()
-
-
-        while (true) {
-            val lastTwo = numbersToLastTwo[lastNumber]!!
-
-            val currentNumber = when {
-                lastTwo.size() < 2 -> 0
-                lastTwo.size() == 2 -> lastTwo.index0!! - lastTwo.index1!!
+            currentNumber = when(lastTwo.size()) {
+                1 -> 0
+                2 -> lastTwo.index0!! - lastTwo.index1!!
                 else -> throw RuntimeException()
             }
 
-            //println("Turn = $turn currentNumber = $currentNumber")
-
-            if (numbersToLastTwo.containsKey(currentNumber)) {
-                numbersToLastTwo[currentNumber]?.add(turn)
-            }
-            else {
-                numbersToLastTwo[currentNumber] = LastTwoV3(index0 = turn, index1 = null)
-            }
-
-//            val jurness = numbersToLastTwo[currentNumber]
-//            numbersToLastTwo[currentNumber] = jurness?.add(turn) ?: LastTwoV3(index0 = turn, index1 = null)
-
-            if (turn == 30_000_000) {
-                return currentNumber
-            }
-
+            val jurness = numbersToLastTwo[currentNumber]
+            numbersToLastTwo[currentNumber] = jurness?.add(turn) ?: LastTwo(index0 = turn, index1 = null)
             turn++
-            lastNumber = currentNumber
         }
+
+        return currentNumber
+    }
+
+    fun solveOne(puzzleText: String): Int {
+        return solve(2020, puzzleText)
+    }
+
+    fun solveTwo(puzzleText: String): Int {
+        return solve(30_000_000, puzzleText)
     }
 }
 
