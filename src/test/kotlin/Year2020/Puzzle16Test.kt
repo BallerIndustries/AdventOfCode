cf37325133c22d1ea035f80a0bd9ef7500204959
Angus Cheng <baller.industries@hotmail.com>
2020-12-16 13:17:52 +0800

    part a gg

--- src/test/kotlin/Year2020/Puzzle16Test.kt
+++ src/test/kotlin/Year2020/Puzzle16Test.kt
@@ -21,9 +21,20 @@ class Puzzle16Test {
 
     @Test
     fun `example part a`() {
-        val puzzleText = ""
+        val puzzleText = "class: 1-3 or 5-7\n" +
+                "row: 6-11 or 33-44\n" +
+                "seat: 13-40 or 45-50\n" +
+                "\n" +
+                "your ticket:\n" +
+                "7,1,14\n" +
+                "\n" +
+                "nearby tickets:\n" +
+                "7,3,47\n" +
+                "40,4,50\n" +
+                "55,2,20\n" +
+                "38,6,12"
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(514579, result)
+        assertEquals(71, result)
     }
 
     @Test
@@ -35,8 +46,50 @@ class Puzzle16Test {
 }
 
 class Puzzle16 {
+    data class Rule(val name: String, val range1: IntRange, val range2: IntRange) {
+
+        fun validValue(value: Int): Boolean {
+            return value in range1 || value in range2
+        }
+
+        fun invalidValue(value: Int) = !validValue(value)
+    }
+
     fun solveOne(puzzleText: String): Int {
-        return 1
+        val (rulesText, yourTicket, nearbyTicketsText) = puzzleText.split("\n\n")
+
+        val rules = parseRules(rulesText)
+        val split = nearbyTicketsText.split("\n")
+        val nearbyTickets = split.subList(1, split.size).map { it.split(",").map { it.toInt() } }
+
+        return nearbyTickets.sumBy { nearbyTicket ->
+            nearbyTicket.sumBy { value ->
+                if (rules.all { rule -> rule.invalidValue(value) }) {
+                    value
+                }
+                else {
+                    0
+                }
+            }
+        }
+
+
+
+//        return 1
+    }
+
+    fun parseRules(rulesText: String): List<Rule> {
+        val rules = rulesText.split("\n").map { text ->
+            val name = text.split(":")[0]
+            val (range1, range2) = text.split(": ")[1].split(" or ").map {
+                val (start, end) = it.split("-").map { it.toInt() }
+                IntRange(start, end)
+            }
+
+            Rule(name, range1, range2)
+        }
+
+        return rules
     }
 
     fun solveTwo(puzzleText: String): Int {
