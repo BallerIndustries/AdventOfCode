b8d92dc8f6544814ce511a1be85dad39eecfc720
Angus Cheng <baller.industries@hotmail.com>
2020-12-19 18:24:22 +0800

    we write a lot of tests, because jack favourite dinner is egg

--- src/test/kotlin/Year2020/Puzzle19Test.kt
+++ src/test/kotlin/Year2020/Puzzle19Test.kt
@@ -39,106 +39,162 @@ class Puzzle19Test {
         assertEquals(2, result)
     }
 
+    val exampleTwo = "42: 9 14 | 10 1\n" +
+            "9: 14 27 | 1 26\n" +
+            "10: 23 14 | 28 1\n" +
+            "1: \"a\"\n" +
+            "11: 42 31\n" +
+            "5: 1 14 | 15 1\n" +
+            "19: 14 1 | 14 14\n" +
+            "12: 24 14 | 19 1\n" +
+            "16: 15 1 | 14 14\n" +
+            "31: 14 17 | 1 13\n" +
+            "6: 14 14 | 1 14\n" +
+            "2: 1 24 | 14 4\n" +
+            "0: 8 11\n" +
+            "13: 14 3 | 1 12\n" +
+            "15: 1 | 14\n" +
+            "17: 14 2 | 1 7\n" +
+            "23: 25 1 | 22 14\n" +
+            "28: 16 1\n" +
+            "4: 1 1\n" +
+            "20: 14 14 | 1 15\n" +
+            "3: 5 14 | 16 1\n" +
+            "27: 1 6 | 14 18\n" +
+            "14: \"b\"\n" +
+            "21: 14 1 | 1 14\n" +
+            "25: 1 1 | 1 14\n" +
+            "22: 14 14\n" +
+            "8: 42\n" +
+            "26: 14 22 | 1 20\n" +
+            "18: 15 15\n" +
+            "7: 14 5 | 1 21\n" +
+            "24: 14 1\n" +
+            "\n" +
+            "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa\n" +
+            "bbabbbbaabaabba\n" +
+            "babbbbaabbbbbabbbbbbaabaaabaaa\n" +
+            "aaabbbbbbaaaabaababaabababbabaaabbababababaaa\n" +
+            "bbbbbbbaaaabbbbaaabbabaaa\n" +
+            "bbbababbbbaaaaaaaabbababaaababaabab\n" +
+            "ababaaaaaabaaab\n" +
+            "ababaaaaabbbaba\n" +
+            "baabbaaaabbaaaababbaababb\n" +
+            "abbbbabbbbaaaababbbbbbaaaababb\n" +
+            "aaaaabbaabaaaaababaa\n" +
+            "aaaabbaaaabbaaa\n" +
+            "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa\n" +
+            "babaaabbbaaabaababbaabababaaab\n" +
+            "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+
     @Test
     fun `example part b`() {
-        val puzzleText = "42: 9 14 | 10 1\n" +
-                "9: 14 27 | 1 26\n" +
-                "10: 23 14 | 28 1\n" +
-                "1: \"a\"\n" +
-                "11: 42 31\n" +
-                "5: 1 14 | 15 1\n" +
-                "19: 14 1 | 14 14\n" +
-                "12: 24 14 | 19 1\n" +
-                "16: 15 1 | 14 14\n" +
-                "31: 14 17 | 1 13\n" +
-                "6: 14 14 | 1 14\n" +
-                "2: 1 24 | 14 4\n" +
-                "0: 8 11\n" +
-                "13: 14 3 | 1 12\n" +
-                "15: 1 | 14\n" +
-                "17: 14 2 | 1 7\n" +
-                "23: 25 1 | 22 14\n" +
-                "28: 16 1\n" +
-                "4: 1 1\n" +
-                "20: 14 14 | 1 15\n" +
-                "3: 5 14 | 16 1\n" +
-                "27: 1 6 | 14 18\n" +
-                "14: \"b\"\n" +
-                "21: 14 1 | 1 14\n" +
-                "25: 1 1 | 1 14\n" +
-                "22: 14 14\n" +
-                "8: 42\n" +
-                "26: 14 22 | 1 20\n" +
-                "18: 15 15\n" +
-                "7: 14 5 | 1 21\n" +
-                "24: 14 1\n" +
-                "\n" +
-                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa\n" +
-                "bbabbbbaabaabba\n" +
-                "babbbbaabbbbbabbbbbbaabaaabaaa\n" +
-                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa\n" +
-                "bbbbbbbaaaabbbbaaabbabaaa\n" +
-                "bbbababbbbaaaaaaaabbababaaababaabab\n" +
-                "ababaaaaaabaaab\n" +
-                "ababaaaaabbbaba\n" +
-                "baabbaaaabbaaaababbaababb\n" +
-                "abbbbabbbbaaaababbbbbbaaaababb\n" +
-                "aaaaabbaabaaaaababaa\n" +
-                "aaaabbaaaabbaaa\n" +
-                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa\n" +
-                "babaaabbbaaabaababbaabababaaab\n" +
-                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
-        val result = puzzle.solveTwo(puzzleText)
+        val result = puzzle.solveTwo(exampleTwo)
         assertEquals(12, result)
     }
 
-    val rulesText = "0: 4 1 5\n" +
-            "1: 2 3 | 3 2\n" +
-            "2: 4 4 | 5 5\n" +
-            "3: 4 5 | 5 4\n" +
-            "4: \"a\"\n" +
-            "5: \"b\""
+    @Test
+    fun `combos for exampleTwo`() {
+        val rulesText = exampleTwo.split("\n\n")[0]
+        val rules = puzzle.parseRules(rulesText)
 
+        with(puzzle.combos(rules, rules[42]!!, "", mutableMapOf())) {
+            println("combos for 42 - $this")
+        }
+
+        with(puzzle.combos(rules, rules[31]!!, "", mutableMapOf())) {
+            println("combos for 31 - $this")
+        }
+    }
+
+    @Test
+    fun `combos for puzzleText`() {
+        val rulesText = puzzleText.split("\n\n")[0]
+        val rules = puzzle.parseRules(rulesText)
+
+        with(puzzle.combos(rules, rules[42]!!, "", mutableMapOf())) {
+            println("combos for 42 - $this")
+        }
+
+        with(puzzle.combos(rules, rules[31]!!, "", mutableMapOf())) {
+            println("combos for 31 - $this")
+        }
+    }
 
     @Test
     fun `text matches rule 5`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(2, puzzle.textMatchesRule(rules, rules[3]!!, "ab", 0))
     }
 
     @Test
     fun `text matches rule 6`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(2, puzzle.textMatchesRule(rules, rules[3]!!, "ba", 0))
     }
 
     @Test
     fun `text matches rule 1`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(1, puzzle.textMatchesRule(rules, rules[4]!!, "a", 0))
     }
 
     @Test
     fun `text matches rule 2`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(1, puzzle.textMatchesRule(rules, rules[5]!!, "b", 0))
     }
 
     @Test
     fun `text does not match rule 3`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(1, puzzle.textMatchesRule(rules, rules[5]!!, "bb", 0))
     }
 
     @Test
     fun `text does not match rule 4`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(1, puzzle.textMatchesRule(rules, rules[4]!!, "aa", 0))
     }
 
     @Test
     fun `text matches rule 7`() {
-        val rules = puzzle.parseRules(rulesText)
+        val rules = puzzle.parseRules("0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\"")
         assertEquals(-1, puzzle.textMatchesRule(rules, rules[5]!!, "c", 0))
     }
 
@@ -180,6 +236,140 @@ class Puzzle19Test {
         val rules = puzzle.parseRules(rulesText)
         assertEquals(6, puzzle.textMatchesRule(rules, rules[0]!!, "aaaabb", 0))
     }
+
+    @Test
+    fun `combos for rules - 1`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[5]!!, "", mutableMapOf())
+        assertEquals(setOf("b"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 2`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[4]!!, "", mutableMapOf())
+        assertEquals(setOf("a"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 3`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[3]!!, "", mutableMapOf())
+        assertEquals(setOf("ab"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 4`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[2]!!, "", mutableMapOf())
+        assertEquals(setOf("aa"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 5`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[1]!!, "", mutableMapOf())
+        assertEquals(setOf("aaab"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 6`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[0]!!, "", mutableMapOf())
+        assertEquals(setOf("aaaabb"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 7`() {
+        val rulesText = "0: 4 1 5 | 3\n" +
+                "1: 2 3\n" +
+                "2: 4 4\n" +
+                "3: 4 5\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[0]!!, "", mutableMapOf())
+        assertEquals(setOf("aaaabb", "ab"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 8`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[3]!!, "", mutableMapOf())
+        assertEquals(setOf("ab", "ba"), combos)
+    }
+
+    @Test
+    fun `combos for rules - 9`() {
+        val rulesText = "0: 4 1 5\n" +
+                "1: 2 3 | 3 2\n" +
+                "2: 4 4 | 5 5\n" +
+                "3: 4 5 | 5 4\n" +
+                "4: \"a\"\n" +
+                "5: \"b\""
+
+        // 1 - ["aaab", "aaba", "bbab", "bbba", "abaa", "baaa", "abbb", "babb"]
+        // 2 - ["aa", "bb"]
+        // 3 - ["ab", "ba"]
+        // 4 - ["a"]
+        // 5 - ["b"]
+
+        val rules = puzzle.parseRules(rulesText)
+        val combos = puzzle.combos(rules, rules[1]!!, "", mutableMapOf())
+        assertEquals(setOf("aaab", "aaba", "bbab", "bbba", "abaa", "baaa", "abbb", "babb"), combos)
+    }
+
+
 }
 
 class Puzzle19 {
@@ -227,6 +417,30 @@ class Puzzle19 {
             //println("matched up to textIndex $textIndex")
             return textIndex
         }
+
+        fun combos(puzzle: Puzzle19, rules: Map<Int, Rule>, prefix: String, memo: MutableMap<Int, Set<String>>): List<String> {
+            var currentStuff = listOf("")
+
+            for (token in tokens) {
+                val (ruleId: Int?, char: Char?) = token
+
+                if (char != null) {
+                    currentStuff = currentStuff.map { it + char.toString() }
+                }
+                else if (ruleId != null) {
+                    currentStuff = currentStuff.flatMap { it ->
+                        puzzle.combos(rules, rules[ruleId]!!, "", memo).map { aaa ->
+                            it + aaa
+                        }
+                    }
+                }
+                else {
+                    throw RuntimeException()
+                }
+            }
+
+            return currentStuff
+        }
     }
 
     data class Rule(val name: Int, val subRules: List<SubRule>) {
@@ -240,6 +454,18 @@ class Puzzle19 {
             }
 
             return -1
+        }
+
+        fun combos(puzzle: Puzzle19, rules: Map<Int, Rule>, prefix: String, memo: MutableMap<Int, Set<String>>): Set<String> {
+            if (memo[name] == null) {
+                val combos = subRules.flatMap { subRule ->
+                    subRule.combos(puzzle, rules, prefix, memo)
+                }.toSet()
+
+                memo[name] = combos
+            }
+
+            return memo[name]!!
         }
     }
 
@@ -307,6 +533,10 @@ class Puzzle19 {
             result
 
         }
+    }
+
+    fun combos(rules: Map<Int, Rule>, rule: Rule, prefix: String, memo: MutableMap<Int, Set<String>>): Set<String> {
+        return rule.combos(this, rules, prefix, memo)
     }
 }
 
