fb26be8fc84e3836c39b55f3731105086f163cc2
Angus Cheng <baller.industries@hotmail.com>
2020-12-20 11:48:16 +0800

    aaa

--- src/test/kotlin/Year2020/Puzzle19Test.kt
+++ src/test/kotlin/Year2020/Puzzle19Test.kt
@@ -354,12 +354,14 @@ class Puzzle19Test {
 
     @Test
     fun `combos for rules - 9`() {
-        val rulesText = "0: 4 1 5\n" +
-                "1: 2 3 | 3 2\n" +
-                "2: 4 4 | 5 5\n" +
-                "3: 4 5 | 5 4\n" +
-                "4: \"a\"\n" +
-                "5: \"b\""
+        val rulesText = """
+            0: 4 1 5
+            1: 2 3 | 3 2
+            2: 4 4 | 5 5
+            3: 4 5 | 5 4
+            4: "a"
+            5: "b"
+            """.trimIndent()
 
         // 1 - ["aaab", "aaba", "bbab", "bbba", "abaa", "baaa", "abbb", "babb"]
         // 2 - ["aa", "bb"]
