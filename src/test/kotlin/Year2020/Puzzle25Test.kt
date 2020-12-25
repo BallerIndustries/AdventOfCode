565a9ab95e3056a72779789fc16a5630c747a887
Angus Cheng <baller.industries@hotmail.com>
2020-12-25 13:24:20 +0800

    gg!

--- src/test/kotlin/Year2020/Puzzle25Test.kt
+++ src/test/kotlin/Year2020/Puzzle25Test.kt
@@ -10,37 +10,68 @@ class Puzzle25Test {
     @Test
     fun `puzzle part a`() {
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(964875, result)
-    }
-
-    @Test
-    fun `puzzle part b`() {
-        val result = puzzle.solveTwo(puzzleText)
-        assertEquals(158661360, result)
+        assertEquals(11288669, result)
     }
 
     @Test
     fun `example part a`() {
-        val puzzleText = ""
+        val puzzleText = "5764801\n17807724"
         val result = puzzle.solveOne(puzzleText)
-        assertEquals(514579, result)
+        assertEquals(14897079, result)
     }
 
     @Test
-    fun `example part b`() {
-        val puzzleText = ""
-        val result = puzzle.solveTwo(puzzleText)
-        assertEquals(241861950, result)
+    fun `determine secret loop size 1`() {
+        val result: Long = puzzle.determineSecretLoopSize(5764801, 7)
+        assertEquals(8, result)
+    }
+
+    @Test
+    fun `determine secret loop size 2`() {
+        val result: Long = puzzle.determineSecretLoopSize(17807724, 7)
+        assertEquals(11, result)
     }
 }
 
 class Puzzle25 {
-    fun solveOne(puzzleText: String): Int {
-        return 1
+    fun solveOne(puzzleText: String): Long {
+        val (cardPublicKey, doorPublicKey) = puzzleText.split("\n").map { it.toLong() }
+
+        val cardSecretLoopSize = determineSecretLoopSize(cardPublicKey, 7)
+        val doorSecretLoopSize = determineSecretLoopSize(doorPublicKey, 7)
+
+        return transformLoop(doorPublicKey, cardSecretLoopSize)
     }
 
-    fun solveTwo(puzzleText: String): Int {
-        return 1
+    fun determineSecretLoopSize(publicKey: Long, subjectNumber: Long): Long {
+        var value = 1L
+        var count = 0L
+
+        while (true) {
+            value = transform(value, subjectNumber)
+            count++
+
+            if (value == publicKey) {
+                return count
+            }
+        }
+    }
+
+    fun transformLoop(subjectNumber: Long, loopSize: Long): Long {
+        var value = 1L
+
+        (0 until loopSize).forEach {
+            value = transform(value, subjectNumber)
+        }
+
+        return value
+    }
+
+    private fun transform(value: Long, subjectNumber: Long): Long {
+        var value1 = value
+        value1 = value1 * subjectNumber
+        value1 = value1 % 20201227
+        return value1
     }
 }
 
