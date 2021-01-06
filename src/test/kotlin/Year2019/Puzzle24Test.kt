3ec63e63883faa18d0bf5ea01e55ec4f121d8377
Angus Cheng <baller.industries@hotmail.com>
2021-01-06 17:02:54 +0800

    flipping stuff around to match the puzzle examples

--- src/test/kotlin/Year2019/Puzzle24Test.kt
+++ src/test/kotlin/Year2019/Puzzle24Test.kt
@@ -58,7 +58,7 @@ class Puzzle24Test {
             Puzzle24.Point3D(x=0, y=2, z=0),
             Puzzle24.Point3D(x=0, y=4, z=0),
             Puzzle24.Point3D(x=1, y=3, z=0),
-            Puzzle24.Point3D(x=4, y=2, z=1),
+            Puzzle24.Point3D(x=4, y=2, z=-1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=0, y=3, z=0).neighbors())
     }
@@ -68,8 +68,8 @@ class Puzzle24Test {
         val expected = setOf(
             Puzzle24.Point3D(x=3, y=0, z=0),
             Puzzle24.Point3D(x=4, y=1, z=0),
-            Puzzle24.Point3D(x=2, y=4, z=1),
-            Puzzle24.Point3D(x=0, y=2, z=1),
+            Puzzle24.Point3D(x=2, y=4, z=-1),
+            Puzzle24.Point3D(x=0, y=2, z=-1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=4, y=0, z=0).neighbors())
     }
@@ -77,8 +77,8 @@ class Puzzle24Test {
     @Test
     fun `0-4-0 neighbors, bottom left corner`() {
         val expected = setOf(
-            Puzzle24.Point3D(x=4, y=2, z=1),
-            Puzzle24.Point3D(x=2, y=0, z=1),
+            Puzzle24.Point3D(x=4, y=2, z=-1),
+            Puzzle24.Point3D(x=2, y=0, z=-1),
 
             Puzzle24.Point3D(x=0, y=3, z=0),
             Puzzle24.Point3D(x=1, y=4, z=0),
@@ -94,11 +94,11 @@ class Puzzle24Test {
             Puzzle24.Point3D(x=1, y=1, z=0),
             Puzzle24.Point3D(x=1, y=3, z=0),
 
-            Puzzle24.Point3D(x=0, y=0, z=-1),
-            Puzzle24.Point3D(x=0, y=1, z=-1),
-            Puzzle24.Point3D(x=0, y=2, z=-1),
-            Puzzle24.Point3D(x=0, y=3, z=-1),
-            Puzzle24.Point3D(x=0, y=4, z=-1),
+            Puzzle24.Point3D(x=0, y=0, z=1),
+            Puzzle24.Point3D(x=0, y=1, z=1),
+            Puzzle24.Point3D(x=0, y=2, z=1),
+            Puzzle24.Point3D(x=0, y=3, z=1),
+            Puzzle24.Point3D(x=0, y=4, z=1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=1, y=2, z=0).neighbors())
     }
@@ -110,11 +110,11 @@ class Puzzle24Test {
             Puzzle24.Point3D(x=3, y=1, z=0),
             Puzzle24.Point3D(x=3, y=3, z=0),
 
-            Puzzle24.Point3D(x=4, y=0, z=-1),
-            Puzzle24.Point3D(x=4, y=1, z=-1),
-            Puzzle24.Point3D(x=4, y=2, z=-1),
-            Puzzle24.Point3D(x=4, y=3, z=-1),
-            Puzzle24.Point3D(x=4, y=4, z=-1),
+            Puzzle24.Point3D(x=4, y=0, z=1),
+            Puzzle24.Point3D(x=4, y=1, z=1),
+            Puzzle24.Point3D(x=4, y=2, z=1),
+            Puzzle24.Point3D(x=4, y=3, z=1),
+            Puzzle24.Point3D(x=4, y=4, z=1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=3, y=2, z=0).neighbors())
     }
@@ -126,11 +126,11 @@ class Puzzle24Test {
             Puzzle24.Point3D(x=3, y=1, z=0),
             Puzzle24.Point3D(x=2, y=0, z=0),
 
-            Puzzle24.Point3D(x=0, y=0, z=-1),
-            Puzzle24.Point3D(x=1, y=0, z=-1),
-            Puzzle24.Point3D(x=2, y=0, z=-1),
-            Puzzle24.Point3D(x=3, y=0, z=-1),
-            Puzzle24.Point3D(x=4, y=0, z=-1),
+            Puzzle24.Point3D(x=0, y=0, z=1),
+            Puzzle24.Point3D(x=1, y=0, z=1),
+            Puzzle24.Point3D(x=2, y=0, z=1),
+            Puzzle24.Point3D(x=3, y=0, z=1),
+            Puzzle24.Point3D(x=4, y=0, z=1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=2, y=1, z=0).neighbors())
     }
@@ -142,11 +142,11 @@ class Puzzle24Test {
             Puzzle24.Point3D(x=1, y=3, z=0),
             Puzzle24.Point3D(x=3, y=3, z=0),
 
-            Puzzle24.Point3D(x=0, y=4, z=-1),
-            Puzzle24.Point3D(x=1, y=4, z=-1),
-            Puzzle24.Point3D(x=2, y=4, z=-1),
-            Puzzle24.Point3D(x=3, y=4, z=-1),
-            Puzzle24.Point3D(x=4, y=4, z=-1),
+            Puzzle24.Point3D(x=0, y=4, z=1),
+            Puzzle24.Point3D(x=1, y=4, z=1),
+            Puzzle24.Point3D(x=2, y=4, z=1),
+            Puzzle24.Point3D(x=3, y=4, z=1),
+            Puzzle24.Point3D(x=4, y=4, z=1),
         )
         assertEquals(expected, Puzzle24.Point3D(x=2, y=3, z=0).neighbors())
     }
@@ -233,11 +233,11 @@ class Puzzle24 {
 
             // Center point
             if (new.x == 2 && new.y == 2) {
-                return range.map { Point3D(x = it, y = 4, z = z - 1) }.toSet()
+                return range.map { Point3D(x = it, y = 4, z = z + 1) }.toSet()
             }
 
             if (new.y == -1) {
-                return setOf(Point3D(x = 2, y = 4, z = z + 1))
+                return setOf(Point3D(x = 2, y = 4, z = z - 1))
             }
 
             return setOf(new)
@@ -249,11 +249,11 @@ class Puzzle24 {
 
             // Center point
             if (new.x == 2 && new.y == 2) {
-                return range.map { Point3D(x = it, y = 0, z = z - 1) }.toSet()
+                return range.map { Point3D(x = it, y = 0, z = z + 1) }.toSet()
             }
 
             if (new.y == 5) {
-                return setOf(Point3D(x = 2, y = 0, z = z + 1))
+                return setOf(Point3D(x = 2, y = 0, z = z - 1))
             }
 
             return setOf(new)
@@ -265,11 +265,11 @@ class Puzzle24 {
 
             // Center point
             if (new.x == 2 && new.y == 2) {
-                return range.map { Point3D(x = 4, y = it, z = z - 1) }.toSet()
+                return range.map { Point3D(x = 4, y = it, z = z + 1) }.toSet()
             }
 
             if (new.x == -1) {
-                return setOf(Point3D(x=4, y=2, z=z+1))
+                return setOf(Point3D(x=4, y=2, z=z-1))
             }
 
             return setOf(new)
@@ -281,11 +281,11 @@ class Puzzle24 {
 
             // Center point
             if (new.x == 2 && new.y == 2) {
-                return range.map { Point3D(x = 0, y = it, z = z - 1) }.toSet()
+                return range.map { Point3D(x = 0, y = it, z = z + 1) }.toSet()
             }
 
             if (new.x == 5) {
-                return setOf(Point3D(x = 0, y = 2, z = z + 1))
+                return setOf(Point3D(x = 0, y = 2, z = z - 1))
             }
 
             return setOf(new)
